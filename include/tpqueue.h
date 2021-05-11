17372d9509b73c1d2d3739be9dab8d3f6e80070c
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-05-11 12:04:11 +0300

    a?

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,12 +5,74 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+    struct ITEM {
+        T data;
+        ITEM* next;
+    };
+
+ public:
+    TPQueue() :head(nullptr), tail(nullptr) {}
+    ~TPQueue() {
+        while (head)
+            pop();
+    }
+    void push(const T& data) {
+        if (tail && head) {
+            ITEM* current = head;
+
+            if (current->data.prior >= data.prior) {
+                while (current->next) {
+                    if (current->next->data.prior < data.prior) {
+                        ITEM* temp = create(data);
+                        temp->next = current->next;
+                        current->next = temp;
+                        break;
+                    }
+                    else {
+                        current = current->next;
+                    }
+                }
+            }
+            else {
+                current = create(data);
+                current->next = head;
+                head = current;
+            }
+
+            if (!current->next) {
+                tail->next = create(data);
+                tail = tail->next;
+            }
+        }
+        else {
+            head = create(data);
+            tail = head;
+        }
+    }
+
+    T pop() {
+        assert(head);
+        T data = head->data;
+        ITEM* temp = head->next;
+        delete head;
+        head = temp;
+
+        return data;
+    }
+ private:
+    TPQueue::ITEM* create(const T& data) {
+        ITEM* item = new ITEM;
+        item->data = data;
+        item->next = nullptr;
+        return item;
+    }
+    ITEM* head;
+    ITEM* tail;
 };
 
 struct SYM {
-  char ch;
-  int  prior;
+    char ch;
+    int  prior;
 };
 
 #endif // INCLUDE_TPQUEUE_H_
