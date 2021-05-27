7a7b2bd97415b6fd7def76b3fc9f4aa203f4516e
koisoi <60881099+koisoi@users.noreply.github.com>
2021-05-27 21:34:00 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,76 +11,76 @@ class TPQueue {
   };
 
    private:
-    ITEM * create(const T &data) {
-    ITEM * item = new ITEM;
-    item->data = data;
-    item->next = nullptr;
-    return item;
-    }
+     ITEM * create(const T &data) {
+     ITEM * item = new ITEM;
+     item->data = data;
+     item->next = nullptr;
+     return item;
+     }
 
-    ITEM * head;
-    ITEM * tail;
+     ITEM * head;
+     ITEM * tail;
 
    public:
-    TPQueue(): head(nullptr), tail(nullptr) {}
+     TPQueue(): head(nullptr), tail(nullptr) {}
 
-    ~TPQueue() {
-      while (head)
-        pop();
-    }
+     ~TPQueue() {
+       while (head)
+         pop();
+     }
 
-    void push(const T& data) {
-      if (!(tail && head)) {
-        head = create(data);
-        tail = head;
-      } else if (tail == head) {
-        if (data.prior > head->data.prior) {
-          auto *temp = head;
-          head = create(data);
-          head->next = temp;
-        } else {
-          head->next = create(data);
-          tail = head->next;
-        }
-      } else {
-        if (data.prior > head->data.prior) {
-          auto *temp = head;
-          head = create(data);
-          head->next = temp;
-        } else {
-          auto *temp = head;
-          while (temp != tail) {
-            if (data.prior > temp->next->data.prior) {
-              ITEM *item = create(data);
-              item->next = temp->next;
-              temp->next = item;
-              break;
-            } else {temp = temp->next;}
-          }
-          if (temp = tail) {
-            tail = create(data);
-            temp->next = tail;
-          }
-        }
-      }
-    }
+     void push(const T& data) {
+       if (!(tail && head)) {
+         head = create(data);
+         tail = head;
+       } else if (tail == head) {
+         if (data.prior > head->data.prior) {
+           auto *temp = head;
+           head = create(data);
+           head->next = temp;
+         } else {
+           head->next = create(data);
+           tail = head->next;
+         }
+       } else {
+         if (data.prior > head->data.prior) {
+           auto *temp = head;
+           head = create(data);
+           head->next = temp;
+         } else {
+           auto *temp = head;
+           while (temp != tail) {
+             if (data.prior > temp->next->data.prior) {
+               ITEM *item = create(data);
+               item->next = temp->next;
+               temp->next = item;
+               break;
+             } else {temp = temp->next;}
+           }
+           if (temp = tail) {
+             tail = create(data);
+             temp->next = tail;
+           }
+         }
+       }
+     }
 
-    T pop() {
-      if (head) {
-        T data = head->data;
-        if (head == tail) {
-          delete head;
-          head = tail = nullptr;
-          return data;
-        } else {
-          ITEM *temp = head->next;
-          delete head;
-          head = temp;
-          return data;
-        }
-      }
-    }
-};
+     T pop() {
+       if (head) {
+         T data = head->data;
+         if (head == tail) {
+           delete head;
+           head = tail = nullptr;
+           return data;
+         } else {
+           ITEM *temp = head->next;
+           delete head;
+           head = temp;
+           return data;
+         }
+       }
+     }
+ };
 
 struct SYM {
   char ch;
