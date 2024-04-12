9eeee1bdaed1cc3fdde7dd9202b453a033d63be4
Vika Zadorozhnaya <150604407+Vviktori@users.noreply.github.com>
2024-04-12 22:55:00 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -6,30 +6,30 @@
 template<typename T, int size>
 class TStack {
  private:
-   T data[100];
-   int perem;
+    T data[100];
+    int perem;
  public:
-   TStack() : perem(-1) {}
-   void push(T item) {
-     if (!isFull)
-       data[++perem] = item;
-     else
-       throw std::string("Full");
-   }
-   bool isFull() const {
-     return perem == -1;
-   }
-   bool empty() const {
+    TStack() : perem(-1) {}
+    void push(T item) {
+      if (!isFull)
+        data[++perem] = item;
+      else
+        throw std::string("Full");
+    }
+    bool isFull() const {
+      return perem == -1;
+    }
+    bool empty() const {
      return perem == 0;
-   }
-   T pop() {
-     if (!empty)
-       return data[--perem];
-     else
-       throw std::string("Empty");
-   }
-   T top() const {
-     return data[perem];
+    }
+    T pop() {
+      if (!empty)
+        return data[--perem];
+      else
+        throw std::string("Empty");
+    }
+    T top() const {
+      return data[perem];
 };
 
 #endif  // INCLUDE_TSTACK_H_
