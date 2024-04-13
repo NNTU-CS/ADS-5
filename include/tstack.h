f0012c284f8ed0bc3fc5d0791842416c1b856e13
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-04-13 12:28:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,35 +3,30 @@
 #define INCLUDE_TSTACK_H_
 #include <string>
 
-template <typename T, int size>
+template<typename T, int size>
 class TStack {
-  private:
-    T arr[100];
-    int top;
-  public:
-    TStack() : top(-1) {}
-    void add(T value) {
-      if (!isFull())
-        arr[++top] = value;
-      else
-        throw std::string("Full");
+private:
+  T data[100];
+  int top;
+
+public:
+  TStack() : top(-1) {}
+  void add(T value) {
+    if (!isFull())
+      data[++top] = value;
+    else
+      throw std::string("Full");
+  }
+  T show() const { return data[top]; }
+  T pop() {
+    if (isEmpty()) {
+      throw std::string("Empty");
+    } else {
+      return data[top--];
     }
-    T show() const { 
-      return arr[top]; 
-    }
-    T pop() {
-      if (isEmpty()) {
-        throw std::string("Empty");
-      } else {
-        return arr[top--];
-      }
-    }
-    bool isEmpty() const { 
-      return top == -1; 
-    }
-    bool isFull() const { 
-      return top == size - 1; 
-    }
+  }
+  bool isEmpty() const { return top == -1; }
+  bool isFull() const { return top == size - 1; }
 };
 
 #endif  // INCLUDE_TSTACK_H_
