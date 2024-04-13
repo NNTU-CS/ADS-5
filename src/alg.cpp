a87c40f37f83fb22fb5ab4a6aa862f6ae6365ec1
VadimKochnev <donskoy_kek@mail.ru>
2024-04-13 15:04:13 +0300

    6

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
-#include "./include/tstack.h"
+#include "tstack.h"
 
 int getPriority(char operand) {
   if (operand == '+' || operand == '-') return 1;
