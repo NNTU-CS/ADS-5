79fef2d09c4f3ec088196ebe74b5d3a4b62c0386
flapik19 <selivanovvlad3211@gmail.com>
2024-04-13 19:25:11 +0300

    idk

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,17 @@
 #include <map>
 #include "tstack.h"
 
-std::map <char, int> prioritet 
-{ 
-  std::make_pair('(', 0), 
-  std::make_pair(')', 1),
-  std::make_pair('+', 2),
-  std::make_pair('-', 2),
-  std::make_pair('*', 3),
-  std::make_pair('/', 3)
-};
+int prioritet(char sym) { 
+  switch (sym) {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': return 2;
+    case '-': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    default: return -1;
+  }
+}
 
 bool isNum(char sym){
   return '0' <= sym & sym <= '9';
@@ -22,32 +24,30 @@ TStack<int, 100> stack2;
 
 std::string infx2pstfx(std::string inf) {
   std::string res;
-  for (auto sym : inf) { 
+
+  for (auto sym : inf) {
     if (isNum(sym)) {
-      res += sym;
-    } else if (sym == '(' || (prioritet[stack1.peek()] < prioritet[sym]) || stack1.isEmpty())  { 
+      res = res + sym + ' ';
+    } else if (sym == '(' || prioritet(stack1.peek()) < prioritet(sym) || stack1.isEmpty()) {
       stack1.push(sym);
-      if (prioritet[stack1.peek()] >= prioritet[sym]) { 
-        while (!(stack1.isEmpty())){
-          res = res + ' ' + stack1.peek();
-          stack1.pop();
-        }
-        stack1.push(sym);
-      }
-    } else if (sym == ')') { 
-      while (stack1.peek() != '(') { 
+    } else if (prioritet((sym) <= prioritet(stack1.peek()))) {
+      while(prioritet(stack1.peek() <= prioritet(sym))) {
+        res = res + ' ' + stack1.peek();
+        stack1.pop();
+      } 
+    } else if (sym == ')') {
+      while (stack1.peek() != '(') {
         res = res + ' ' + stack1.peek();
         stack1.pop();
       }
+    }
+    }
+    while (!(stack1.isEmpty())) {
+      res = res + ' ' + stack1.peek();
       stack1.pop();
     }
-  }
-  while (!(stack1.isEmpty())) {
-    res = res + ' ' + stack1.peek();
-    stack1.pop();
-    }
   return res;
-}
+  }
 
 int eval(std::string pref) {
   // добавьте код
