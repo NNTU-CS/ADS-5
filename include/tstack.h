1cfac1a79e7161558a1720111cf20fd0ace3d348
andsvee <160344384+andsvee@users.noreply.github.com>
2024-04-14 23:51:10 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,52 +1,43 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
 
 template<typename T, int size>
 class TStack {
   // добавьте код стека
  private:
-    T *stackArray;
-    int topIndex;
+    T* ptr;
+    int x;
 
  public:
-    TStack() {
-        stackArray = new T[size];
-        topIndex = -1;
+    TStack() : x(0) {
+        ptr = new T[size];
     }
-
-    ~TStack() {
-        delete[] stackArray;
-    }
-
-    void push(T item) {
-        if (topIndex != size - 1) {
-            topIndex++;
-            stackArray[topIndex] = item;
-        }
-    }
-
-    T pop() {
-        if (topIndex != -1) {
-            T element = stackArray[topIndex];
-            topIndex--;
-            return element;
+    void pushup(const T& c) {
+        if (size - 1 >= x) {
+            ptr[x++] = c;
         } else {
-            return T();
+            throw std::string("Fall !");
         }
     }
-
-    T get() const {
-        if (topIndex == -1) {
-            return T();
+    T popback() {
+        if (x > 0) {
+            return ptr[--x];
         } else {
-            return stackArray[topIndex];
+            throw std::string("Empty");
         }
     }
-
-    bool isempty() const {
-        return topIndex == -1;
+    bool IfZero() const {
+        return x == 0;
+    }
+    T ElemUp() const {
+        if (x > 0) {
+            return ptr[x - 1];
+        } else {
+            throw std::string("Fall!");
+        }
     }
 
 };
