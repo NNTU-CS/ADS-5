8ac548cfe32b79834045c36ab2340180083b05e6
andsvee <160344384+andsvee@users.noreply.github.com>
2024-04-14 23:46:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,82 +1,98 @@
 // Copyright 2021 NNTU-CS
-#include <string>
 #include <map>
 #include "tstack.h"
-int prioritet(char a) {
-  if (a == '+' || a == '-')
-    return 1;
-  if (a == '*' || a == '/')
-    return 2;
-  return 0;
+
+bool isOperator(char op) {
+    return (op == '+' || op == '-' || op == '(' ||
+        op == ')' || op == '/' || op == '*');
+}
+bool isDigit(char n) {
+    return (n >= '0' && n <= '9');
 }
 
-bool isZnak(char a) {
-  return (a == '+' || a == '-' || a == '*' || a == '/');
+int whatPrioritet(char op) {
+    if (op == '-' || op == '+')
+        return 1;
+    if (op == '/' || op == '*')
+        return 2;
+    return 0;
 }
 std::string infx2pstfx(std::string inf) {
   // добавьте код
   return std::string("");
-  std::string inf2;
-  TStack<char, 100> stack1;
-  for (char i : inf) {
-    if (i >= '0' && i <= '9') {
-      inf2 = inf2 + i + ' ';
-    } else if (i == '(') {
-      stack1.push(i);
-    } else if (isZnak(i)) {
-      while (!stack1.isEmpty() && prioritet(stack1.get()) >= prioritet(i)) {
-        inf2 = inf2 + stack1.get() + ' ';
-        stack1.pop();
-      }
-      stack1.push(i);
-    } else if (i == ')') {
-      while (!stack1.isEmpty() && stack1.get() != '(') {
-        inf2 = inf2 + stack1.get() + ' ';
-        stack1.pop();
-      }
-      stack1.pop();
+    std::string post;
+    int c = 0;
+    TStack <char, 100> stack1;
+    for (char s : inf) {
+        if (isDigit(s)) {
+            c++;
+            if (c == 1) {
+                post += s;
+                continue;
+            }
+            post = post + ' ' + s;
+        } else if (isOperator(s)) {
+            if (s == '(') {
+                stack1.push(s);
+            } else if (stack1.checkEmpty()) {
+                stack1.push(s);
+            } else if (whatPrioritet(s) > whatPrioritet(stack1.get())) {
+                stack1.push(s);
+            } else if (s == ')') {
+                while (stack1.get() != '(') {
+                    post = post + ' ' + stack1.get();
+                    stack1.pop();
+                }
+                stack1.pop();
+            } else {
+                int x = whatPrioritet(s);
+                int y = whatPrioritet(stack1.get());
+                while (!stack1.checkEmpty() && x <= y) {
+                    post = post + ' ' + stack1.get();
+                    stack1.pop();
+                }
+                stack1.push(s);
+            }
+        }
     }
-  }
-  while (!stack1.isEmpty()) {
-    inf2 = inf2 + stack1.get() + ' ';
-    stack1.pop();
-  }
-  if (!inf2.empty()) {
-    inf2.pop_back();
-  }
-  return inf2;
+    while (!stack1.checkEmpty()) {
+        post = post + ' ' + stack1.get();
+        stack1.pop();
+    }
+    return post;
 }
-
 
 int eval(std::string pref) {
   // добавьте код
   return 0;
-  TStack<int, 100> stack2;
-  for (char i : pref) {
-    if (i >= '0' && i <= '9') {
-      stack2.push(i - '0');
-    } else if (isZnak(i)) {
-      int x = stack2.get();
-      stack2.pop();
-      int y = stack2.get();
-      stack2.pop();
-      switch (i) {
-        case '+':
-          stack2.push(y + x);
-          break;
-        case '-':
-          stack2.push(y - x);
-          break;
-        case '*':
-          stack2.push(y * x);
-          break;
-        case '/':
-          stack2.push(y / x);
-          break;
-        default:
-          continue;
-      }
+    TStack <int, 100> stack2;
+    for (char s : pref) {
+        if (isDigit(s)) {
+            stack2.push(s - '0');
+        } else if (isOperator(s)) {
+            int x = stack2.get();
+            stack2.pop();
+            int y = stack2.get();
+            stack2.pop();
+            switch (s) {
+            case '+':
+                stack2.push(x + y);
+                break;
+            case '-':
+                stack2.push(y - x);
+                break;
+            case '*':
+                stack2.push(x * y);
+                break;
+            case '/':
+                stack2.push(y / x);
+                break;
+            default:
+                continue;
+            }
+        } else {
+            continue;
+        }
     }
-  }
-  return stack2.get();
+    return stack2.get();
 }
