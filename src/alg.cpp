bce633637fd4b6a499f8adbc96f90ec58263a7c7
AntiCreep753 <160477169+AntiCreep753@users.noreply.github.com>
2024-04-20 11:08:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,118 +1,91 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
 #include <string>
 #include <map>
 #include "tstack.h"
 
-bool isOperation(const char c) {
-    const std::string operations = "()+-*/";
-    return operations.find(c) != std::string::npos;
+int Priority(char c) {
+  if (c == '(')
+    return 0;
+  else if (c == ')')
+    return 1;
+  else if (c == '+' || c == '-')
+    return 2;
+  else if (c == '*' || c == '/')
+    return 3;
+  else
+    return -1;
 }
-
-bool isOperand(const char c) {
-    return !isOperation(c);
-}
-
-int getPriority(char c) {
-    switch (c) {
-    case '(': return 0;
-    case ')': return 1;
-    case '+':
-    case '-': return 2;
-    case '*':
-    case '/': return 3;
-    default: return -1;
-    }
-}
-
-std::string appendOperand(std::string* post, const std::string& operand) {
-    if (operand.empty()) {
-        return "";
-    }
-    if (!post->empty()) {
-        post->append(" ");
-    }
-    post->append(operand);
-    return "";
-}
-
-void appendOperation(std::string* post, TStack<char, 100>* stack) {
-    if (stack->get() != '(') {
-        if (!post->empty()) {
-            post->append(" ");
-        }
-        post->append(std::string(1, stack->get()));
-    }
-    stack->pop();
-}
-
-int calculate(int a, char o, int b) {
-    switch (o) {
-    case '+': return a + b;
-    case '-': return a - b;
-    case '*': return a * b;
-    case '/': return a / b;
-    default: std::cout << "unkown operation: " << o << std::endl;
-        exit(-1);
-    }
-}
-
-
 std::string infx2pstfx(std::string inf) {
-  std::string post = "";
-    std::string operand = "";
-    TStack<char, 100> stack;
-    for (auto c : inf) {
-        if (isOperand(c)) {
-            operand += c;
-            continue;
-        }
-        operand = appendOperand(&post, operand);
-        int priority;
-        priority = getPriority(c);
-        if (priority == 0 || stack.isEmpty() ||
-            priority > getPriority(stack.get())) {
-        } else {
-            while (!stack.isEmpty() && getPriority(stack.get()) >= priority) {
-                appendOperation(&post, &stack);
+std::string post;
+TStack<char, 100> stack1;
+for (char c : inf) {
+if (Priority(c) == -1) {
+  post += c;
+  post += ' ';
+} else {
+    if (c == ')') {
+      while (stack1.get() != '(') {
+        post += stack1.pop();
+        post += ' ';
+      }
+      stack1.pop();
+    } else if (c == '(' || stack1.isEmpty()) {
+        stack1.push(c);
+      } else if (!stack1.isEmpty()) {
+          char elem = stack1.get();
+          if (Priority(elem) < Priority(c)) {
+            stack1.push(c);
+          } else {
+              while (Priority(elem) >= Priority(c) && !stack1.isEmpty()) {
+                post += stack1.pop();
+                post += ' ';
+              }
+              stack1.push(c);
             }
-            if (stack.get() == '(') {
-                stack.pop();
-            }
         }
-        if (c != ')') {
-            stack.push(c);
-        }
-    }
-    operand = appendOperand(&post, operand);
-    while (!stack.isEmpty()) {
-        appendOperation(&post, &stack);
-    }
-    return post;
+  }
 }
-
-int eval(std::string pref) {
- TStack<int, 100> stack;
-    std::string s("");
-    for (auto c : pref) {
-        if (isOperation(c)) {
-            int b = stack.get();
-            stack.pop();
-            int a = stack.get();
-            stack.pop();
-            int result = calculate(a, c, b);
-            stack.push(result);
-            continue;
-        }
-        if (c == ' ') {
-            if (s.size() != 0) {
-                int o = atoi(s.c_str());
-                s.clear();
-                stack.push(o);
-            }
-        } else {
-            s += c;
-        }
+while (!stack1.isEmpty()) {
+post += stack1.pop();
+post += ' ';
+}
+post.pop_back();
+return post;
+}
+int eval(std::string post) {
+TStack<int, 100> stack2;
+std::string number;
+for (int i = 0; i < post.size(); ++i) {
+if (Priority(post[i]) == -1) {
+  if (post[i] == ' ') {
+    continue;
+  } else if (isdigit(post[i + 1])) {
+      number += post[i];
+      continue;
+    } else {
+        number += post[i];
+        int value = atoi(number.c_str());
+        stack2.push(value);
+        number = "";
+      }
+} else {
+    int b = stack2.pop();
+    int a = stack2.pop();
+    switch (post[i]) {
+    case '+':
+      stack2.push(a + b);
+      break;
+    case '-':
+      stack2.push(a - b);
+      break;
+    case '*':
+      stack2.push(a * b);
+      break;
+    case '/':
+      stack2.push(a / b);
+      break;
     }
-    return stack.get();
+  }
+}
+return stack2.get();
 }
