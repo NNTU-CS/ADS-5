ad01d185b36b2aefdd930eef1238da840c4968a2
pavuko <99765321+pavuko@users.noreply.github.com>
2025-04-09 18:53:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T* arr;
+  int top;
+public:
+  TStack() {
+    arr = new T[size];
+    top = -1;
+  }
+  bool isEmpty()const { 
+    return top == -1;
+  }
+  bool isFull() const { 
+    return top == size - 1;
+  }
+  void push(T& el) {
+    if (!isFull()) {
+      arr[++top] = el;
+    }
+  }
+  T get() const {
+    if (!isEmpty()) {
+      return arr[top];
+  }
+  }
+  void pop() {
+    if (!isEmpty()) {
+      --top;
+  }
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
