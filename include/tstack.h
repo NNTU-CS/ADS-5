6a3f5827293ab0c4b011fd7079dc154f08372de4
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-04-10 19:23:51 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,35 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[size];
+    int top;
+public:
+    TStack() : top(-1) {}
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
+    void push(const T& value) {
+        if (!isFull()) {
+            data[++top] = value;
+        }
+    }
+    T pop() {
+        if (!isEmpty()) {
+            return data[top--];
+        }
+        throw std::out_of_range("Stack is empty");
+    }
+    T peek() const {
+        if (!isEmpty()) {
+            return data[top];
+        }
+        throw std::out_of_range("Stack is empty");
+    }
 };
-
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
