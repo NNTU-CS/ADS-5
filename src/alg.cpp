563f74c0216ce23cba6239f95a8f47192c650c06
nefedovat <ntanya54321@gmail.com>
2025-04-11 18:50:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,33 +5,32 @@
 
 template <typename T, int SIZE>
 class TStack {
-private:
+ private:
   T data[SIZE];
   int topIndex;
-
-public:
-  TStack() : topIndex(-1) {}
-
+ public:
+  TStack() : topIndex(-1) {
+    for (int i = 0; i < SIZE; ++i) {
+      data[i] = T();
+    }
+  }
   void push(const T& value) {
     if (topIndex < SIZE - 1) {
       data[++topIndex] = value;
     }
   }
-
   T pop() {
     if (topIndex >= 0) {
       return data[topIndex--];
     }
     return T();
   }
-
   T top() const {
     if (topIndex >= 0) {
       return data[topIndex];
     }
     return T();
   }
-
   bool isEmpty() const {
     return topIndex == -1;
   }
@@ -44,10 +43,8 @@ std::string infx2pstfx(const std::string& inf) {
     {'+', 1}, {'-', 1},
     {'*', 2}, {'/', 2}
   };
-
   for (size_t i = 0; i < inf.size(); ++i) {
     char c = inf[i];
-    
     if (isdigit(c)) {
       while (i < inf.size() && isdigit(inf[i])) {
         result += inf[i++];
@@ -63,31 +60,26 @@ std::string infx2pstfx(const std::string& inf) {
       }
       stack.pop();
     } else if (precedence.count(c)) {
-      while (!stack.isEmpty() && stack.top() != '(' && 
-             precedence[stack.top()] >= precedence[c]) {
+      while (!stack.isEmpty() && stack.top() != '(' && precedence[stack.top()] >= precedence[c]) {
         result += stack.pop();
         result += ' ';
       }
       stack.push(c);
     }
   }
-
   while (!stack.isEmpty()) {
     result += stack.pop();
     result += ' ';
   }
-
   if (!result.empty() && result.back() == ' ') {
     result.pop_back();
   }
-
   return result;
 }
 
 int eval(const std::string& post) {
   TStack<int, 100> stack;
   std::string numStr;
-
   for (char c : post) {
     if (isdigit(c)) {
       numStr += c;
@@ -97,7 +89,6 @@ int eval(const std::string& post) {
     } else if (c == '+' || c == '-' || c == '*' || c == '/') {
       int right = stack.pop();
       int left = stack.pop();
-      
       switch (c) {
         case '+': stack.push(left + right); break;
         case '-': stack.push(left - right); break;
@@ -106,6 +97,5 @@ int eval(const std::string& post) {
       }
     }
   }
-
   return stack.pop();
 }
