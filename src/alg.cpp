1e0c76365f8ffd70e274cda3775fbbc2b6628435
BushSasha <sashabush2006@gmail.com>
2025-04-11 14:17:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2025 NNTU-CS
 #include <string>
-#include <map>
-#include "tstack.h"
 #include <sstream>
 #include <cctype>
+#include <map>
+#include "tstack.h"
 
 int getPriorityOp(char op) {
-    switch(op) {
+    switch (op) {
         case '+': case '-': return 1;
         case '*': case '/': return 2;
         default: return 0;
@@ -16,7 +16,7 @@ std::string infx2pstfx(const std::string& inf) {
     TStack<char, 100> stack;
     std::stringstream result;
     std::string number;
-    
+
     for (char c : inf) {
         if (isdigit(c)) {
             number += c;
@@ -34,7 +34,8 @@ std::string infx2pstfx(const std::string& inf) {
                 }
                 stack.pop(); // Удаляем '(' из стека
             } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                while (!stack.isEmpty() && getPriorityOp(stack.peek()) >= getPriorityOp(c)) {
+                while (!stack.isEmpty() &&
+                    getPriorityOp(stack.peek()) >= getPriorityOp(c)) {
                     result << stack.pop() << " ";
                 }
                 stack.push(c);
@@ -45,11 +46,11 @@ std::string infx2pstfx(const std::string& inf) {
     if (!number.empty()) {
         result << number << " ";
     }
-    
+
     while (!stack.isEmpty()) {
         result << stack.pop() << " ";
     }
-    
+
     std::string outputPos = result.str();
     if (!outputPos.empty() && outputPos.back() == ' ') {
         outputPos.pop_back();
@@ -61,15 +62,15 @@ int eval(const std::string& pref) {
     TStack<int, 100> stack;
     std::stringstream ss(post);
     std::string token;
-    
+
     while (ss >> token) {
         if (isdigit(token[0])) {
             stack.push(std::stoi(token));
         } else {
             int b = stack.pop();
             int a = stack.pop();
-            
-            switch(token[0]) {
+
+            switch (token[0]) {
                 case '+': stack.push(a + b); break;
                 case '-': stack.push(a - b); break;
                 case '*': stack.push(a * b); break;
@@ -79,5 +80,3 @@ int eval(const std::string& pref) {
     }
   return stack.pop();
 }
-
-
