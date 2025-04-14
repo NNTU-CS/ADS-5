bfe8352a9087782ec6318c0a9d5020b7d35a1ae0
methevodichka <145225062+methevodichka@users.noreply.github.com>
2025-04-14 19:49:31 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,42 +5,42 @@
 
 template<typename T, int kStackSize>
 class TStack {
-  private:
-     T data[kStackSize];
-     int topIndex;
+ private:
+    T data[kStackSize];
+    int topIndex;
 
-  public:
-     TStack() : topIndex(-1) {}
-     bool empty() const {
-       return topIndex < 0;
-     }
-     bool full() const {
-       return topIndex >= kStackSize - 1;
-     }
-     void push(const T& item) {
-     if (full()) {
-       throw std::runtime_error("Error: Stack overflow");
-     }
-       data[++topIndex] = item;
-     }
-     void pop() {
-       if (empty()) {
-         throw std::runtime_error("Error: stack is empty");
-       }
-       --topIndex;
-     }
-     T& top() {
-       if (empty()) {
-         throw std::runtime_error("Error: stack is empty");
-       }
-       return data[topIndex];
-     }
-     const T& top() const {
-       if (empty()) {
-         throw std::runtime_error("Error: stack is empty");
-       }
-       return data[topIndex];
-     }
+public:
+    TStack() : topIndex(-1) {}
+    bool empty() const {
+        return topIndex < 0;
+    }
+    bool full() const {
+        return topIndex >= kStackSize - 1;
+    }
+    void push(const T& item) {
+    if (full()) {
+      throw std::runtime_error("Error: Stack overflow");
+    }
+      data[++topIndex] = item;
+    }
+    void pop() {
+        if (empty()) {
+            throw std::runtime_error("Error: stack is empty");
+        }
+        --topIndex;
+    }
+    T& top() {
+        if (empty()) {
+            throw std::runtime_error("Error: stack is empty");
+        }
+        return data[topIndex];
+    }
+    const T& top() const {
+        if (empty()) {
+            throw std::runtime_error("Error: stack is empty");
+        }
+        return data[topIndex];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
