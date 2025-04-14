8ba459c62e4b31bf91ebcdd74680a810c703254e
KorobovaKate <kat3kor@yandex.ru>
2025-04-14 23:08:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,89 +14,94 @@ int prioritet(char op) {
     }
 }
 
-bool isOperator(char c) {
+bool opperator(char c) {
     return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
 std::string infx2pstfx(const std::string& inf) {
-    TStack<char, 100> stack;
+    TStack<char, 100> stack1;
     std::ostringstream result;
     bool flag = false;
     for (size_t i = 0; i < inf.size(); ++i) {
         char c = inf[i];
         if (std::isspace(c)) continue;
         if (std::isdigit(c)) {
-            if (flag) {
+            if (flag)
+            {
                 result << ' ';
             }
-            while (i < inf.size() && std::isdigit(inf[i])) {
+            while (i < inf.size() && std::isdigit(inf[i]))
+            {
                 result << inf[i++];
             }
+            i--;
             flag = true;
         }
-        else if (c == '(') {
-            stack.push(c);
+        else if (c == '(')
+        {
+            stack1.push(c);
             flag = false;
-        }
-        else if (c == ')') {
-            while (!stack.isEmpty() && stack.peek() != '(') {
-                result << ' ' << stack.pop();
+        } else if (c == ')')
+        {
+            while (!stack1.isEmpty() && stack1.peek() != '(')
+            {
+                result << ' ' << stack1.pop();
             }
-            if (stack.isEmpty()) {
-                throw std::invalid_argument("Mismatched parentheses");
+            if (!stack1.isEmpty() && stack1.peek() == '(')
+            {
+                stack1.pop();
             }
-            stack.pop(); // Удаляем '('
             flag = true;
-        }
-        else if (isOperator(c)) {
-            while (!stack.isEmpty() && stack.peek() != '(' && prioritet(c) <= prioritet(stack.peek())) {
-                result << ' ' << stack.pop();
+        } else if (opperator(c))
+        {
+            while (!stack1.isEmpty() && stack1.peek() != '(' && prioritet(c) <= prioritet(stack1.peek()))
+            {
+                result << ' ' << stack1.pop();
             }
-            stack.push(c);
+            stack1.push(c);
             flag = true;
-        }
-        else {
+        } else {
             throw std::invalid_argument("Invalid character");
         }
     }
-    while (!stack.isEmpty()) {
-        if (stack.peek() == '(') {
-            throw std::invalid_argument("Mismatched parentheses");
-        }
-        result << ' ' << stack.pop();
+    while (!stack1.isEmpty())
+    {
+        result << ' ' << stack1.pop();
     }
     return result.str();
 }
 
 int eval(const std::string& post) {
-    TStack<int, 100> stack;
+    TStack<int, 100> stack2;
     std::istringstream iss(post);
     std::string token;
-    while (iss >> token) {
-        if (std::isdigit(token[0])) {
-            stack.push(std::stoi(token));
-        }
-        else if (isOperator(token[0]) && token.size() == 1) {
-            if (stack.isEmpty()) throw std::invalid_argument("Not enough operands");
-            int right = stack.pop();
-            if (stack.isEmpty()) throw std::invalid_argument("Not enough operands");
-            int left = stack.pop();
-            switch (token[0]) {
-                case '+': stack.push(left + right); break;
-                case '-': stack.push(left - right); break;
-                case '*': stack.push(left * right); break;
+    while (iss >> token)
+    {
+        if (std::isdigit(token[0]))
+        {
+            stack2.push(std::stoi(token));
+        } else if (opperator(token[0]) && token.size() == 1)
+        {
+            if (stack2.isEmpty()) throw std::invalid_argument("Not enough operands");
+            int right = stack2.pop();
+            if (stack2.isEmpty()) throw std::invalid_argument("Not enough operands");
+            int left = stack2.pop();
+            switch (token[0])
+            {
+                case '+': stack2.push(left + right); break;
+                case '-': stack2.push(left - right); break;
+                case '*': stack2.push(left * right); break;
                 case '/':
                     if (right == 0) throw std::runtime_error("Division by zero");
-                    stack.push(left / right);
+                    stack2.push(left / right);
                     break;
             }
-        }
-        else {
+        } else {
             throw std::invalid_argument("Invalid token");
         }
     }
-    if (stack.isEmpty()) throw std::invalid_argument("Empty expression");
-    int result = stack.pop();
-    if (!stack.isEmpty()) throw std::invalid_argument("Too many operands");
+    if (stack2.isEmpty()) throw std::invalid_argument("Empty expression");
+    int result = stack2.pop();
+    if (!stack2.isEmpty()) throw std::invalid_argument("Too many operands");
     return result;
 }
