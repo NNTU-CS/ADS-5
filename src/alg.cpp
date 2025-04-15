ef7e1d025ce68d52e7ce95f58fc0fde0f74ffdf5
TechnoMagicc <leyzarof@gmail.com>
2025-04-15 15:21:26 +0300

    v 1.0

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,65 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
+std::string infx2pstfx(const std::string& inf) {
+    std::stack<char> stack;
+    std::string result;
+    std::istringstream iss(inf);
+    std::string token;
 
+    while (iss >> token) {
+        if (isdigit(token[0])) {
+            result += token + " ";
+        } else if (token[0] == '(') {
+            stack.push('(');
+        } else if (token[0] == ')') {
+            while (!stack.empty() && stack.top() != '(') {
+                result += stack.top();
+                result += " ";
+                stack.pop();
+            }
+            stack.pop();
+        } else {
+            while (!stack.empty() && precedence(stack.top()) >= precedence(token[0])) {
+                result += stack.top();
+                result += " ";
+                stack.pop();
+            }
+            stack.push(token[0]);
+        }
+    }
+}
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+    std::stack<int> stack;
+    std::istringstream iss(post);
+    std::string token;
+
+    while (iss >> token) {
+        if (isdigit(token[0])) {
+            stack.push(std::stoi(token));
+        } else {
+            int right = stack.top(); stack.pop();
+            int left = stack.top(); stack.pop();
+            switch (token[0]) {
+                case '+':
+                    stack.push(left + right);
+                    break;
+                case '-':
+                    stack.push(left - right);
+                    break;
+                case '*':
+                    stack.push(left * right);
+                    break;
+                case '/':
+                    stack.push(left / right);
+                    break;
+            }
+        }
+    }
+    return stack.top();
 }
