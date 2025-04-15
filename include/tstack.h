e213dd484c6f40d5c38ff16e42aff51d5a86042d
pootaatoovarenaya <lilechekT@mail.ru>
2025-04-15 22:56:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,36 +1,51 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int kStackSize>
+#include <array>
+#include <stdexcept>
+#include <string>
+
+template<typename T, int size>
 class TStack {
-  private:
-    T data[kStackSize];
-    int topIndex;
-  
-  public:
-    TStack() : topIndex(-1) {}
+ public:
+    TStack() : top(-1) {}
+
+    bool isEmpty() const noexcept {
+        return top == -1;
+    }
+
+    bool isFull() const noexcept {
+        return top == size - 1;
+    }
+
     void push(const T& value) {
-      if (topIndex < size - 1) {
-        data[++topIndex] = value;
-      }
+        if (isFull()) {
+            throw std::overflow_error("Stack overflow");
+        }
+        elements[++top] = value;
     }
-    void pop() {
-      if (!empty()) {
-        topIndex--;
-      }
+
+    T pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack underflow");
+        }
+        return elements[top--];
     }
-    T top() const {
-      if (!empty()) {
-        return data[topIndex];
-      }
-      return T();
+
+    const T& peek() const {
+        if (isEmpty()) {
+            throw std::logic_error("Stack is empty");
+        }
+        return elements[top];
     }
-    bool empty() const {
-      return topIndex == -1;
+
+    void clear() noexcept {
+        top = -1;
     }
-    bool isFull() const {
-      return topIndex == size - 1;
-    }
+
+ private:
+    std::array<T, size> elements;
+    int top;
 };
+
 #endif  // INCLUDE_TSTACK_H_
