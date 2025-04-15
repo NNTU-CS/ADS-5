f7c76eaae149c7662a0230735ca57d0a6e82f9f8
pootaatoovarenaya <lilechekT@mail.ru>
2025-04-15 22:46:58 +0300

    TTTTT

--- include/tstack.h
+++ include/tstack.h
@@ -2,34 +2,35 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int kStackSize>
 class TStack {
-private:
-T data[size];
-int topIndex;
-public:
-TStack() : topIndex(-1) {}
-void push(const T& value) {
-  if (topIndex < size - 1) {
-    data[++topIndex] = value;
-  }
-}
-void pop() {
-  if (!empty()) {
-    topIndex--;
-  }
-}
-T top() const {
-  if (!empty()) {
-    return data[topIndex];
-  }
-  return T();
-}
-bool empty() const {
-  return topIndex == -1;
-}
-bool isFull() const {
-  return topIndex == size - 1;
-}
+  private:
+    T data[kStackSize];
+    int topIndex;
+  
+  public:
+    TStack() : topIndex(-1) {}
+    void push(const T& value) {
+      if (topIndex < size - 1) {
+        data[++topIndex] = value;
+      }
+    }
+    void pop() {
+      if (!empty()) {
+        topIndex--;
+      }
+    }
+    T top() const {
+      if (!empty()) {
+        return data[topIndex];
+      }
+      return T();
+    }
+    bool empty() const {
+      return topIndex == -1;
+    }
+    bool isFull() const {
+      return topIndex == size - 1;
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
