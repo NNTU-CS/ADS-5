2e93dd37dcda9508c2fa07730181f1b19b95b9ea
antihackss <vladmakov0859@gmail.com>
2025-04-16 13:31:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,9 +5,7 @@
 #include <cctype>
 #include "tstack.h"
 
-using namespace std;
-
-int getPriority(char op) {
+static int GetPriority(char op) {
   if (op == '+' || op == '-') return 1;
   if (op == '*' || op == '/') return 2;
   return 0;
@@ -15,70 +13,81 @@ int getPriority(char op) {
 
 std::string infx2pstfx(const std::string& inf) {
   TStack<char, 100> stack;
-    stringstream postfix;
-    bool prevWasDigit = false;
+  std::stringstream postfix;
+  bool prev_was_digit = false;
 
-    for (size_t i = 0; i < inf.size(); ++i) {
-      char c = inf[i];
+  for (size_t i = 0; i < inf.size(); ++i) {
+    char c = inf[i];
 
-        if (isspace(c)) {
-          continue;
-        }
-
-        if (isdigit(c)) {
-          if (prevWasDigit) {
-            postfix << c;
-          } else {
-            if (i != 0) postfix << ' ';
-              postfix << c;
-          }
-          prevWasDigit = true;
-        } else {
-          prevWasDigit = false;
-
-          if (c == '(') {
-            stack.push(c);
-          } else if (c == ')') {
-            while (!stack.isEmpty() && stack.peek() != '(') {
-              postfix << ' ' << stack.pop();
-            }
-              stack.pop();
-            } else {
-              while (!stack.isEmpty() && getPriority(stack.peek()) >= getPriority(c)) {
-                postfix << ' ' << stack.pop();
-              }
-              stack.push(c);
-            }
-        }
+    if (isspace(c)) {
+      continue;
     }
 
-    while (!stack.isEmpty()) {
-      postfix << ' ' << stack.pop();
-    }
+    if (isdigit(c)) {
+      if (prev_was_digit) {
+        postfix << c;
+      } else {
+        if (i != 0) {
+          postfix << ' ';
+        }
+        postfix << c;
+      }
+      prev_was_digit = true;
+    } else {
+      prev_was_digit = false;
 
-    return postfix.str();
+      if (c == '(') {
+        stack.push(c);
+      } else if (c == ')') {
+        while (!stack.empty() && stack.top() != '(') {
+          postfix << ' ' << stack.pop();
+        }
+        stack.pop();
+      } else {
+        while (!stack.empty() &&
+               GetPriority(stack.top()) >= GetPriority(c)) {
+          postfix << ' ' << stack.pop();
+        }
+        stack.push(c);
+      }
+    }
+  }
+
+  while (!stack.empty()) {
+    postfix << ' ' << stack.pop();
+  }
+
+  return postfix.str();
 }
 
-int eval(const std::string& pref) {
+int eval(const std::string& post) {
   TStack<int, 100> stack;
-    stringstream ss(post);
-    string token;
+  std::stringstream ss(post);
+  std::string token;
 
-    while (ss >> token) {
-      if (isdigit(token[0])) {
-        stack.push(stoi(token));
-      } else {
-          int right = stack.pop();
-          int left = stack.pop();
-            
-          switch (token[0]) {
-            case '+': stack.push(left + right); break;
-            case '-': stack.push(left - right); break;
-            case '*': stack.push(left * right); break;
-            case '/': stack.push(left / right); break;
-          }
-        }
+  while (ss >> token) {
+    if (isdigit(token[0])) {
+      stack.push(std::stoi(token));
+    } else {
+      int right = stack.pop();
+      int left = stack.pop();
+
+      switch (token[0]) {
+        case '+':
+          stack.push(left + right);
+          break;
+        case '-':
+          stack.push(left - right);
+          break;
+        case '*':
+          stack.push(left * right);
+          break;
+        case '/':
+          stack.push(left / right);
+          break;
+      }
     }
+  }
 
-    return stack.pop();
+  return stack.pop();
 }
