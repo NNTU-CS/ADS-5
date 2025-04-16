f6e7432e337ef78b8d92f20e8c5274cff98c9b50
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-16 23:04:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,82 @@
 // Copyright 2025 NNTU-CS
 #include <string>
 #include <map>
+#include <iostream>
+#include <sstream>
 #include "tstack.h"
 
 std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+    std::string result;
+TStack<char, 100> opStack;
+std::stringstream ss(inf);
+std::string token;
+std::map<char, int> precedence;
+precedence['+'] = 1;
+precedence['-'] = 1;
+precedence['*'] = 2;
+precedence['/'] = 2;
+while (ss >> token) {
+if (isdigit(token[0])) {
+result += token + " ";
+}
+else if (token == "(") {
+opStack.push(token[0]);
+}
+else if (token == ")") {
+while (!opStack.empty() && opStack.top() != '(') {
+result += opStack.top();
+result += " ";
+opStack.pop();
+}
+opStack.pop();
+}
+else {
+while (!opStack.empty() && precedence[opStack.top()] >= precedence[token[0]]) {
+result += opStack.top();
+result += " ";
+opStack.pop();
+}
+opStack.push(token[0]);
+}
+}
+while (!opStack.empty()) {
+result += opStack.top();
+result += " ";
+opStack.pop();
+}
+result.pop_back(); 
+return result;
 }
 
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+TStack<int, 100> operandStack;
+std::stringstream ss(post);
+std::string token;
+while (ss >> token) {
+if (isdigit(token[0])) {
+int num = 0;
+for (char c : token) {
+num = num * 10 + (c - '0');
+}
+operandStack.push(num);
+} else {
+int operand2 = operandStack.top();
+operandStack.pop();
+int operand1 = operandStack.top();
+operandStack.pop();
+if (token == "+") {
+operandStack.push(operand1 + operand2);
+}
+else if (token == "-") {
+operandStack.push(operand1 - operand2);
+}
+else if (token == "*") {
+operandStack.push(operand1 * operand2);
+}
+else if (token == "/") {
+operandStack.push(operand1 / operand2);
+}
+}
+}
+return operandStack.top();
 }
