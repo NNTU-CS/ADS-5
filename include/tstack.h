1c8f89c94f0214631448f2294da4af801b66d2c0
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-04-16 21:32:33 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,32 +2,43 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template <typename T, int size>
+template <typename T, int Size>
 class TStack {
-private:
-    T data[size];
-    int top;
-public:
-    TStack() : top(-1) {}
 
-    void push(const T& value) {
-        if (top >= size - 1) throw std::overflow_error("Stack overflow");
-        data[++top] = value;
-    }
+ private:
+  T arr[Size];
+  int top;
 
-    T pop() {
-        if (top < 0) throw std::underflow_error("Stack underflow");
-        return data[top--];
-    }
+ public:
+  TStack() : top(-1) { }
 
-    T peek() const {
-        if (top < 0) throw std::underflow_error("Stack is empty");
-        return data[top];
+  void push(const T& val) {
+    if (top < Size - 1) {
+      arr[++top] = val;
     }
+  }
 
-    bool isEmpty() const {
-        return top == -1;
+  T pop() {
+    if (top >= 0) {
+      return arr[top--];
     }
+    return T();
+  }
+
+  T peek() const {
+    if (top >= 0) {
+      return arr[top];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return top == -1;
+  }
+
+  bool isFull() const {
+    return top == Size - 1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
