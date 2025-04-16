abc76150ede17169eab3660041bbab692393033d
Павел Золин <pavelzolin@MacBook-Pro-Pavel.local>
2025-04-16 21:44:23 +0300

    реализовал задание

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,45 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <stdexcept>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
-};
+private:
+  T elements[size];
+  int currentIndex;
 
+public:
+  TStack() : currentIndex(-1) {}
+  bool empty() const {
+    return currentIndex < 0;
+  }
+  bool full() const {
+    return currentIndex >= size - 1;
+  }
+  void push(const T& value) {
+    if (full()) {
+      throw std::runtime_error("Error: Stack overflow");
+    }
+    elements[++currentIndex] = value;
+  }
+  void pop() {
+    if (empty()) {
+      throw std::runtime_error("Error: stack is empty");
+    }
+    --currentIndex;
+  }
+  T& top() {
+    if (empty()) {
+      throw std::runtime_error("Error: stack is empty");
+    }
+    return elements[currentIndex];
+  }
+  const T& top() const {
+    if (empty()) {
+      throw std::runtime_error("Error: stack is empty");
+    }
+    return elements[currentIndex];
+  }
+};
 #endif  // INCLUDE_TSTACK_H_
