43228948143952c622fdcab3f12cb611dc22efd0
antihackss <vladmakov0859@gmail.com>
2025-04-16 13:38:10 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+  T data[size];
+  int top;
+
+ public:
+  TStack() : top(-1) {}
+
+  bool isEmpty() const {
+    return top == -1;
+  }
+
+  bool isFull() const {
+    return top == size - 1;
+  }
+
+  void push(T value) {
+    if (!isFull()) {
+      data[++top] = value;
+    }
+  }
+
+  T pop() {
+    if (!isEmpty()) {
+      return data[top--];
+    }
+    return T();
+  }
+
+  T peek() const {
+    if (!isEmpty()) {
+      return data[top];
+    }
+    return T();
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
