10cdd897e5e714281c8bd285640b8c33efff9a02
munderr <mvyazov534@gmail.com>
2025-04-16 22:45:31 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,35 +1,35 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <stdexcept>
 
-template<typename T, int size>
+template <typename T, int kSize>
 class TStack {
-private:
-    T data[size];
-    int top;
-public:
-    TStack() : top(-1) {}
-    void push(const T& value) {
-        if (top >= size - 1)
-            throw std::overflow_error("Stack overflow");
-        data[++top] = value;
-    }
-    void pop() {
-        if (isEmpty())
-            throw std::underflow_error("Stack underflow");
-        --top;
-    }
-    T get() const {
-        if (isEmpty())
-            throw std::underflow_error("Stack underflow");
-        return data[top];
-    }
-    bool isEmpty() const {
-        return top == -1;
-    }
-    int count() const {
-        return top + 1;
-    }
+ private:
+  T data[kSize];
+  int top;
+ public:
+  TStack() : top(-1) {}
+  void push(const T& value) {
+    if (top >= kSize - 1)
+      throw std::overflow_error("Stack overflow");
+    data[++top] = value;
+  }
+  void pop() {
+    if (isEmpty())
+      throw std::underflow_error("Stack underflow");
+    --top;
+  }
+  T get() const {
+    if (isEmpty())
+      throw std::underflow_error("Stack underflow");
+    return data[top];
+  }
+  bool isEmpty() const {
+    return top == -1;
+  }
+  int count() const {
+    return top + 1;
+  }
 };
-};
-#endif  // INCLUDE_TSTACK_H_
+#endif
