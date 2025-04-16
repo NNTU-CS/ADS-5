a88ed60498636663f0fa41ec68922c3ffd1fa828
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-16 23:12:14 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,45 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <stdexcept>
 
-template<typename T, int size>
+template<typename T, int MSSize>
 class TStack {
-  // добавьте код стека
+private:
+T data_[MSSize];
+size_t size_ = 0;
+
+public:
+bool empty() const { return size_ == 0; }
+size_t size() const { return size_; }
+
+void push(const T& value) {
+if (size_ == MSSize) {
+throw std::out_of_range("Stack overflow");
+}
+data_[size_++] = value;
+}
+
+T& top() {
+if (empty()) {
+throw std::out_of_range("Stack is empty");
+}
+return data_[size_ - 1];
+}
+
+const T& top() const {
+if (empty()) {
+throw std::out_of_range("Stack is empty");
+}
+return data_[size_ - 1];
+}
+
+void pop() {
+if (empty()) {
+throw std::out_of_range("Stack is empty");
+}
+--size_;
+}
 };
 
 #endif  // INCLUDE_TSTACK_H_
