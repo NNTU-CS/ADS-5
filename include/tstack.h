3c6a79757bbdfa09a490ec8aa8bb85b71ab6a4a7
Эль шейх <elina_shibanova@mail.ru>
2025-04-17 12:31:54 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,31 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[size];
+    int top;
+public:
+    TStack() : top(-1) {}
+    void Push(T value) {
+        if (top < size - 1) {
+            data[++top] = value;
+        }
+    }
+    T Pop() {
+        if (!IsEmpty()) {
+            return data[top--];
+        }
+        return T();
+    }
+    T Top() {
+        if (!IsEmpty()) {
+            return data[top];
+        }
+        return T();
+    }
+    bool IsEmpty() {
+        return top == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
