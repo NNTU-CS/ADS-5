6f295b371ad9de96c231f7a9810a9f99e2636190
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-04-17 15:46:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,44 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T arr[size];
+    int top;
+
+ public:
+    TStack() {
+        top = -1;
+    }
+
+    bool isEmpty() const {
+        return top == -1;
+    }
+
+    bool isFull() const {
+        return top == size - 1;
+    }
+
+    void push(const T& value) {
+        if (isFull()) {
+            throw std::string("Stack is full!");
+        }
+        arr[++top] = value;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw std::string("Stack is empty!");
+        }
+        return arr[top--];
+    }
+
+    T get() const {
+        if (isEmpty()) {
+            throw std::string("Stack is empty!");
+        }
+        return arr[top];
+    }
+};
 };
 
 #endif  // INCLUDE_TSTACK_H_
