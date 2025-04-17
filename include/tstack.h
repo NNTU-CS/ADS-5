cfdda9628b85c4d9af31c50fe441e6a975cbf0d8
Эль шейх <elina_shibanova@mail.ru>
2025-04-17 12:42:41 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,30 +2,34 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, constexpr int size>
 class TStack {
-private:
     T data[size];
     int top;
+
 public:
     TStack() : top(-1) {}
+
     void Push(T value) {
         if (top < size - 1) {
             data[++top] = value;
         }
     }
+
     T Pop() {
         if (!IsEmpty()) {
             return data[top--];
         }
         return T();
     }
+
     T Top() {
         if (!IsEmpty()) {
             return data[top];
         }
         return T();
     }
+
     bool IsEmpty() {
         return top == -1;
     }
