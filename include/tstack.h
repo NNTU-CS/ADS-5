03f87612a2fd9bca8b10de2559f872526a8008ce
AstrakhantsevaNastya <anastasia.astr1904@gmail.com>
2025-04-17 22:09:31 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,41 +4,41 @@
 
 template<typename T, int size>
 class TStack {
-  private:
-    static constexpr int kStackSize = size;
-    T m_data[kStackSize];
-    int m_index;
+ private:
+   static constexpr int kStackSize = size;
+   T m_data[kStackSize];
+   int m_index;
+
+   public:
+     TStack() : m_index(-1) {}
   
-  public:
-    TStack() : m_index(-1) {}
+     void push(const T& value) {
+       if (m_index < kStackSize - 1) {
+         m_data[++m_index] = value;
+       }
+     }
   
-    void push(const T& value) {
-      if (m_index < kStackSize - 1) {
-        m_data[++m_index] = value;
-      }
-    }
+     T pop() {
+       if (m_index >= 0) {
+         return m_data[m_index--];
+       }
+       return T();
+     }
   
-    T pop() {
-      if (m_index >= 0) {
-        return m_data[m_index--];
-      }
-      return T();
-    }
+     T top() const {
+       if (m_index >= 0) {
+         return m_data[m_index];
+       }
+       return T();
+     }
   
-    T top() const {
-      if (m_index >= 0) {
-        return m_data[m_index];
-      }
-      return T();
-    }
+     bool isEmpty() const {
+       return m_index == -1;
+     }
   
-    bool isEmpty() const {
-      return m_index == -1;
-    }
-  
-    bool isFull() const {
-      return m_index == kStackSize - 1;
-    }
+     bool isFull() const {
+       return m_index == kStackSize - 1;
+     }
 };
 
 #endif  // INCLUDE_TSTACK_H_
