186b83047fdf1beb2f84eba505b71e87e0f8f1be
Julia-Mamaeva <julia0601alex@icloud.com>
2025-04-17 13:17:56 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,45 +1,43 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#include <stdexcept>
+#include <string>
 
-template<typename ElementType, int kCapacity>
-class CustomStack {
+template<typename DataType, int MaxSize>
+class TStack {
  private:
-    ElementType stackStorage[kCapacity];
-    int currentPosition;
+    DataType elementsStorage[100];
+    int stackPointer;
 
  public:
-    CustomStack() : currentPosition(-1) {}
-    bool isStackEmpty() const {
-        return currentPosition < 0;
+    TStack() : stackPointer(-1) {}
+
+    void push(DataType valueToAdd) {
+      if (isFull()) {
+        throw std::string("Stack is full!");
+      } else {
+        elementsStorage[++stackPointer] = valueToAdd;
+      }
     }
-    bool isStackFull() const {
-        return currentPosition >= kCapacity - 1;
+
+    const DataType& pop() {
+      if (isEmpty()) {
+        throw std::string("Stack is empty!");
+      } else {
+        return elementsStorage[stackPointer--];
+      }
     }
-    void addElement(const ElementType& newItem) {
-        if (isStackFull()) {
-            throw std::runtime_error("Stack capacity exceeded");
-        }
-        stackStorage[++currentPosition] = newItem;
+
+    bool isEmpty() const {
+      return stackPointer == -1;
     }
-    void removeElement() {
-        if (isStackEmpty()) {
-            throw std::runtime_error("Cannot remove from empty stack");
-        }
-        --currentPosition;
+
+    bool isFull() const {
+      return stackPointer == MaxSize - 1;
     }
-    ElementType& peekTop() {
-        if (isStackEmpty()) {
-            throw std::runtime_error("Cannot access empty stack");
-        }
-        return stackStorage[currentPosition];
-    }
-    const ElementType& peekTop() const {
-        if (isStackEmpty()) {
-            throw std::runtime_error("Cannot access empty stack");
-        }
-        return stackStorage[currentPosition];
+
+    const DataType& get() const {
+      return elementsStorage[stackPointer];
     }
 };
 #endif  // INCLUDE_TSTACK_H_
