f141291492914e1e879a2186f5f2747f4cbf7658
1POMI1 <ptsumr@gmail.com>
2025-04-17 13:44:23 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,36 +1,31 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
-  private:
-    T data[size];  // NOLINT(runtime/arrays)
-    int top;
+ public:
+  TStack() : top(-1) {}
 
-  public:
-    TStack() : top(-1) {}
+  void push(const T& val) {
+    if (top < size - 1)
+      data[++top] = val;
+  }
 
-    void push(const T& val) {
-        if (top < size - 1)
-            data[++top] = val;
-    }
+  T pop() {
+    if (!isEmpty())
+      return data[top--];
+    return T();
+  }
 
-    T pop() {
-        if (!isEmpty())
-            return data[top--];
-        return T();
-    }
+  T peek() const {
+    if (!isEmpty())
+      return data[top];
+    return T();
+  }
 
-    T peek() const {
-        if (!isEmpty())
-            return data[top];
-        return T();
-    }
+  bool isEmpty() const {
+    return top == -1;
+  }
 
-    bool isEmpty() const {
-        return top == -1;
-    }
+ private:
+  T data[size];  // NOLINT(runtime/arrays)
+  int top;
 };
-
-#endif  // INCLUDE_TSTACK_H_
