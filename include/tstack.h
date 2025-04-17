0b622ae8b5f52c5f710af3935666394c4e716907
AstrakhantsevaNastya <anastasia.astr1904@gmail.com>
2025-04-17 21:07:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,37 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  static constexpr int stackSize = size;
+  T data[stackSize];
+  int tIndex;
+
+public:
+  TStack() : tIndex(-1) {}
+
+  void push(const T& value) {
+    if (tIndex < stackSize - 1) {
+      data[tIndex++] = value;
+    }
+  }
+
+  T pop() {
+    if (tIndex >= 0) {
+      return data[tIndex--];
+    }
+    return T();
+  }
+
+  T top() const {
+    if (tIndex >= 0) {
+      return data[tIndex];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return tIndex == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
