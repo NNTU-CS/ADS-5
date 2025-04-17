6f427afac13d99b3a6fca86fb6b34f71e395764e
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-04-17 21:33:04 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,36 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[size];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  void push(const T& value) {
+    if (topIndex < size - 1) {
+      data[++topIndex] = value;
+    }
+  }
+
+  void pop() {
+    if (topIndex >= 0) {
+      --topIndex;
+    }
+  }
+
+  T& top() {
+    return data[topIndex];
+  }
+
+  const T& top() const {
+    return data[topIndex];
+  }
+
+  bool isEmpty() const {
+    return topIndex == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
