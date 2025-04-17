c08cc920bfa0e78a37cc09e6a8114bf01a8f49f0
41kur1nka <121097596+41kur1nka@users.noreply.github.com>
2025-04-17 16:43:37 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,40 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+#include <stdexcept>
+
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[size];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  void push(const T& value) {
+    if (full()) throw std::overflow_error("stack overflow");
+    date[++topIndex] = value;
+  }
+
+  void pop() {
+    if (empty()) throw std::underflow_error("stack underflow");
+    --topIndex;
+  }
+
+  T& top() {
+    if (empty()) throw std::underflow_error("stack is empty");
+    return date[topIndex];
+  }
+
+  const T& top() const {
+    if (empty()) throw std::underflow_error("stack is empty");
+    return date[topIndex];
+  }
+
+  int size() const { return topIndex + 1 };
+  bool empty() const { return topIndex == -1 };
+  bool full() const {return topIndex == size - 1}
 };
 
 #endif  // INCLUDE_TSTACK_H_
