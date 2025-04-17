33751d1e52c97af4a7d80c8a4a62500272420165
Эль шейх <elina_shibanova@mail.ru>
2025-04-17 12:35:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,59 @@
 // Copyright 2025 NNTU-CS
-#include <string>
-#include <map>
 #include "tstack.h"
+#include <string>
+#include <cctype>
+#include <sstream>
+using namespace std;
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int prioritet(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+string infx2pstfx(const string& inf) {
+    TStack<char, 100> op_stack;
+    string output;
+    size_t i = 0;
+    while (i < inf.size()) {
+        char c = inf[i];
+        if (isdigit(c)) {
+            string number;
+            while (i < inf.size() && isdigit(inf[i])) {
+                number += inf[i];
+                i++;
+            }
+            output += number + " ";
+        }
+        else if (c == '(') {
+            op_stack.Push(c);
+            i++;
+        }
+        else if (c == ')') {
+            while (!op_stack.IsEmpty() && op_stack.Top() != '(') {
+                output += op_stack.Pop() + " ";
+            }
+            op_stack.Pop();
+            i++;
+        }
+        else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!op_stack.IsEmpty() && op_stack.Top() != '(' && prioritet(c) <= priotitet(op_stack.Top())) {
+                output += op_stack.Pop() + " ";
+            }
+            op_stack.Push(c);
+            i++;
+        }
+        else {
+            i++;
+        }
+    }
+    while (!op_stack.IsEmpty()) {
+        output += op_stack.Pop() + " ";
+    }
+    if (!output.empty()) {
+        output.pop_back();
+    }
+    return output;
 }
 
 int eval(const std::string& pref) {
