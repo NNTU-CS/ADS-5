286c926ea98182e3e2a3302dc6bdf1b49cd4f37d
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-04-17 15:45:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,47 +3,6 @@
 #include <map>
 #include "tstack.h"
 
-template <typename T, int size>
-class TStack {
-private:
-    T arr[size];
-    int top;
-
-public:
-    TStack() {
-        top = -1;
-    }
-
-    bool isEmpty() const {
-        return top == -1;
-    }
-
-    bool isFull() const {
-        return top == size - 1;
-    }
-
-    void push(const T& value) {
-        if (isFull()) {
-            throw std::string("Stack is full!");
-        }
-        arr[++top] = value;
-    }
-
-    T pop() {
-        if (isEmpty()) {
-            throw std::string("Stack is empty!");
-        }
-        return arr[top--];
-    }
-
-    T get() const {
-        if (isEmpty()) {
-            throw std::string("Stack is empty!");
-        }
-        return arr[top];
-    }
-};
-
 int priority(char sym) {
     switch (sym) {
         case '(': return 0;
