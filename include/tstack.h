046462d92ca8f61fa2bc56d8dc0e6efbaa5f4a47
Andrey20-07 <mr.klach999@gmail.com>
2025-04-17 22:14:49 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T arr[size];
+  int index;
+public:
+  Tstack() : index(-1){}
+  bool isempty() const {
+    return index < 0;
+  }
+  bool isfull() const {
+    return index > size - 1;
+  }
+  void push(const T& elem) {
+    if (isfull()) {
+      throw std::runtime_error("Stack is full");
+    }
+    arr[++index] = elem;
+  }
+  void pop() {
+    if (isempty()) {
+      throw std::runtime_error("Stack is empty");
+    }
+    index--;
+  }
+  const T& top() const {
+    if (isempty()) {
+      throw std::runtime_error("Stack is empty");
+    }
+    return arr[index];
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
