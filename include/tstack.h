1a950e9ebf4211828383986633090ffc543d7eb0
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-04-17 16:29:17 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,20 +1,16 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#include<string
+#include<string>
 
 template<typename T, int size>
 class TStack {
   private:
-    T arr[size] = {100};
+    T arr[size] = { 100 };
     int top = -1;
  public:
-  bool isEmpty() const {
-      return top == -1;
-  }
-  bool isFull() const {
-      return top == size - 1;
-  }
+  bool isEmpty() const { return top == -1; }
+  bool isFull() const { return top == size - 1; }
   void push(const T& value) {
       if (isFull()) {
           throw std::string("Stack is full!");
