a8dd6d2c1a62b28d760dfc1273f899ecbc69843e
Arseny <grigoriyevvv2@mail.ru>
2025-04-18 22:36:02 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,41 +1,44 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#include <string>
-
-template<typename T, int size>
+#include <stdexcept>
+template<typename T, int K>
 class TStack {
  private:
-  static constexpr int dataSize = size;
-  T data[dataSize];
-  int top;
+    T data[K];
+    int top;
 
  public:
-  Tstack() : top(-1) {}
-  bool isEmpty() const {
-    return top == -1;
-  }
-  bool isFull() const {
-    return top == size - 1;
-  }
-  T get() const {
-    if (!isEmpty())
-      return data[top];
-    else
-      throw std::string("Empty");
-  }
-  void push(const T& item) {
-    if (!isFull())
-      data[++top] = item;
-    else
-      throw std::string("Full");
-  }
-  void pop() {
-    if (!isEmpty())
-      return data[top--];
-    else
-      throw std::string("Empty");
-  }
+    TStack() : top(-1) {}
+    bool isEmpty() const {
+        return top < 0;
+    }
+    bool isFull() const {
+        return top >= K - 1;
+    }
+    void push(const T& item) {
+        if (isFull()) {
+            throw std::runtime_error("Full");
+        }
+        data[++top] = item;
+    }
+    void pop() {
+        if (isEmpty()) {
+            throw std::runtime_error("Empty");
+        }
+        --top;
+    }
+    T& top() {
+        if (isEmpty()) {
+            throw std::runtime_error("Empty");
+    }
+        return data[top];
+    }
+    const T& top() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Empty");
+        }
+        return data[top];
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
