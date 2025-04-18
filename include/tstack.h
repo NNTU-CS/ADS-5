f3bdab4583b433c734f11691614600482ac4f1a9
danyabatow <danya.batov06@mail.ru>
2025-04-18 16:05:29 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,37 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
   // добавьте код стека
+ private:
+  static constexpr int kStackSize = size;
+  T data[kStackSize];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  void push(const T& value) {
+    if (topIndex < kStackSize - 1) {
+      data[++topIndex] = value;
+    }
+  }
+
+  T pop() {
+    if (topIndex >= 0) {
+      return data[topIndex--];
+    }
+    return T();
+  }
+
+  T top() const {
+    if (topIndex >= 0) {
+      return data[topIndex];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return topIndex == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
