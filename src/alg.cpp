782e828bfd889954b6e4bdc55c0d5c0685f7ac7c
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-18 14:00:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,76 +1,98 @@
 // Copyright 2025 NNTU-CS
-#include <string>
 #include <map>
-#include <iostream>
-#include <sstream>
 #include "tstack.h"
 
+int priority(const char c) {
+switch (c) {
+case '+':
+case '-':
+return 1;
+case '*':
+case '/':
+return 2;
+default:
+return 0;
+}
+}
+
 std::string infx2pstfx(const std::string& inf) {
-    std::string result;
-TStack<char, 100> opStack;
-std::stringstream ss(inf);
-std::map<char, int> precedence;
-precedence['+'] = 1;
-precedence['-'] = 1;
-precedence['*'] = 2;
-precedence['/'] = 2;
-while (ss >> token) {
-std::string token;  
-if (isdigit(token[0])) {
-result += token + " ";
-} else if (token == "(") {
-opStack.push(token[0]);
-} else if (token == ")") {
-while (!opStack.empty() && opStack.top() != '(') {
-result += opStack.top();
-result += " ";
-opStack.pop();
+std::string post;
+TStack<char, 100> stack;
+bool needSpace = false;
+for (int i = 0; i < inf.size(); i++) {
+char current = inf[i];
+int currentPrior = priority(current);
+if (currentPrior == -1) {
+if (needSpace) {
+post += ' ';
 }
-opStack.pop();
+post += current;
+needSpace = false;
+} else if (current == '(') {
+stack.push(current);
+needSpace = false;
+} else if (current == ')') {
+while (!stack.isEmpty() && stack.get() != '(') {
+post += ' ';
+post += stack.get();
+stack.pop();
+}
+stack.pop();
+needSpace = true;
 } else {
-while (!opStack.empty() && precedence[opStack.top()] >= precedence[token[0]]) {
-result += opStack.top();
-result += " ";
-opStack.pop();
+while (!stack.isEmpty() &&
+priority(stack.get()) >= currentPrior &&
+stack.get() != '(') {
+post += ' ';
+post += stack.get();
+stack.pop();
 }
-opStack.push(token[0]);
+post += ' ';
+stack.push(current);
+needSpace = false;
 }
 }
-while (!opStack.empty()) {
-result += opStack.top();
-result += " ";
-opStack.pop();
+while (!stack.isEmpty()) {
+post += ' ';
+post += stack.get();
+stack.pop();
 }
-result.pop_back();
-return result;
+return post;
 }
 
 int eval(const std::string& post) {
-TStack<int, 100> operandStack;
-std::stringstream ss(post);
-while (ss >> token) {
-std::string token;
-if (isdigit(token[0])) {
-int num = std::accumulate(token.begin(),
-token.end(), 0, [](int acc, char c) {
-return acc * 10 +  (c - '0');
-});
-operandStack.push(num);
-} else {
-int operand2 = operandStack.top();
-operandStack.pop();
-int operand1 = operandStack.top();
-operandStack.pop();
-if (token == "+") {
-operandStack.push(operand1 + operand2);
-} else if (token == "-") {
-operandStack.push(operand1 - operand2);
-} else if (token == "*") {
-operandStack.push(operand1 * operand2);
-} else if (token == "/") {
-operandStack.push(operand1 / operand2);
+TStack<int, 100> stack;
+std::string number = "";
+for (size_t i = 0; i < post.length(); i++) {
+char c = post[i];
+if (isdigit(c)) {
+number += c;
+}
+else if (c == ' ') {
+if (!number.empty()) {
+stack.push(std::stoi(number));
+number = "";
+}
+}
+else if (priority(c) >= 2) {
+if (!stack.isEmpty()) {
+int b = stack.get();
+stack.pop();
+if (!stack.isEmpty()) {
+int a = stack.get();
+stack.pop();
+switch (c) {
+case '+': stack.push(a + b); break;
+case '-': stack.push(a - b); break;
+case '*': stack.push(a * b); break;
+case '/': stack.push(a / b); break;
 }
 }
 }
-return operandStack.top();
+}
+}
+if (!number.empty()) {
+stack.push(std::stoi(number));
+}
+return stack.get();
 }
