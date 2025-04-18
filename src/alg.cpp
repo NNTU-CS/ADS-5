aebcbcb67d48551a4649f64a853ee2780c0330f4
danyabatow <danya.batov06@mail.ru>
2025-04-18 15:36:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,43 @@
 // Copyright 2025 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+
+#include <stdexcept>
 
 template <typename T, int Size>
 class TStack {
- private:
-  T arr[Size];
-  int top;
- public:
-  TStack() : top(-1) { }
-  void push(const T& val) {
-    if (top < Size - 1) {
-      arr[++top] = val;
+private:
+    T arr[Size];
+    int top;
+public:
+    TStack() : top(-1) {}
+    void push(const T& val) {
+        if (isFull()) {
+            throw std::overflow_error("Stack is full");
+        }
+        arr[++top] = val;
     }
-  }
-  T pop() {
-    if (top >= 0) {
-      return arr[top--];
+    T pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return arr[top--];
     }
-    return T();
-  }
-  T peek() const {
-    if (top >= 0) {
-      return arr[top];
+    const T& peek() const {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return arr[top];
     }
-    return T();
-  }
-  bool isEmpty() const {
-    return top == -1;
-  }
-  bool isFull() const {
-    return top == Size - 1;
-  }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == Size - 1;
+    }
+    int size() const {
+        return top + 1;
+    }
 };
+
+#endif  // INCLUDE_TSTACK_H_
