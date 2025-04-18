26915f7efb0830713eb8d1af0984735c457441f5
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-18 12:52:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,7 +9,9 @@ std::string infx2pstfx(const std::string& inf) {
     std::string result;
 TStack<char, 100> opStack;
 std::stringstream ss(inf);
+while( ss >> token) {
 std::string token;
+}
 std::map<char, int> precedence;
 precedence['+'] = 1;
 precedence['-'] = 1;
@@ -18,19 +20,16 @@ precedence['/'] = 2;
 while (ss >> token) {
 if (isdigit(token[0])) {
 result += token + " ";
-}
-else if (token == "(") {
+} else if (token == "(") {
 opStack.push(token[0]);
-}
-else if (token == ")") {
+} else if (token == ")") {
 while (!opStack.empty() && opStack.top() != '(') {
 result += opStack.top();
 result += " ";
 opStack.pop();
 }
 opStack.pop();
-}
-else {
+} else {
 while (!opStack.empty() && precedence[opStack.top()] >= precedence[token[0]]) {
 result += opStack.top();
 result += " ";
@@ -44,20 +43,20 @@ result += opStack.top();
 result += " ";
 opStack.pop();
 }
-result.pop_back(); 
+result.pop_back();
 return result;
 }
 
-int eval(const std::string& pref) {
+int eval(const std::string& post) {
 TStack<int, 100> operandStack;
 std::stringstream ss(post);
-std::string token;
 while (ss >> token) {
+std::string token;
 if (isdigit(token[0])) {
-int num = 0;
-for (char c : token) {
-num = num * 10 + (c - '0');
-}
+int num = std::accumulate(token.begin(),
+token.end(), 0, [](int acc, char c) {
+return acc * 10 +  (c - '0');
+});
 operandStack.push(num);
 } else {
 int operand2 = operandStack.top();
@@ -66,14 +65,11 @@ int operand1 = operandStack.top();
 operandStack.pop();
 if (token == "+") {
 operandStack.push(operand1 + operand2);
-}
-else if (token == "-") {
+} else if (token == "-") {
 operandStack.push(operand1 - operand2);
-}
-else if (token == "*") {
+} else if (token == "*") {
 operandStack.push(operand1 * operand2);
-}
-else if (token == "/") {
+} else if (token == "/") {
 operandStack.push(operand1 / operand2);
 }
 }
