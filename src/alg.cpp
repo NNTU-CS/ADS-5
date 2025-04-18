0b67fc6a9533d653fb16f41e10d02cd1dc060a4a
AnnaApatina <annaapatina2002@gmail.com>
2025-04-18 20:11:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,64 @@
 #include <map>
 #include "tstack.h"
 
+int getPriority(char op) {
+  switch (op) {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': case '-': return 2;
+    case '*': case '/': return 3;
+    default: return -1;
+  }
+}
+
+void top2postfix(TStack<char>& stack, std::string& postfix) {
+  postfix += stack.top();
+  stack.pop();
+}
+
 std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+  TStack<char> stack;
+  std::string postfix;
+  for (char ch : inf) {
+    if (isalnum(ch)) {
+      postfix += ch;
+    } else if (getPriority(ch) == 0) {
+      stack.push(ch);
+    } else if (getPriority(ch) == 1) {
+      while (!stack.isEmpty() && getPriority(stack.top()) != 0) {
+        top2postfix(stack, postfix);
+      }
+      stack.pop();
+    } else {
+      while (!stack.isEmpty() && getPriority(stack.top()) >= getPriority(ch)) {
+        top2postfix(stack, postfix);
+      }
+      stack.push(ch);
+    }
+  }
+  while (!stack.isEmpty()) {
+    top2postfix(stack, postfix);
+  }
+  return postfix;
 }
 
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+  std::stack<int> stack;
+  for (char ch : postfix) {
+    if (isdigit(ch)) {
+      stack.push(ch - '0');
+    } else {
+      int b = stack.top();
+      stack.pop();
+      int a = stack.top();
+      stack.pop();
+      switch (ch) {
+        case '+': stack.push(a + b); break;
+        case '-': stack.push(a - b); break;
+        case '*': stack.push(a * b); break;
+        case '/': stack.push(a / b); break;
+      }
+    }
+  }
+  return stack.top();
 }
