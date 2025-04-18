747c2bbcdc1bb52c262bc26b04e28c7c1a5d8637
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-18 14:20:32 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,16 +1,21 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <vector>
 
 template<typename T, int MSSize>
 class TStack {
  private:
-T arr[MSSize];
+std::vector<T> arr;
 int top;
  public:
-TStack() :top(-1) {}
+TStack() : top(-1), arr(MSSize) {}
 T get() const {
+if (top >= 0) {
 return arr[top];
+} else {
+throw std::out_of_range("Stack is empty");
+}
 }
 bool isFull() const {
 return top == MSSize - 1;
@@ -18,13 +23,19 @@ return top == MSSize - 1;
 bool isEmpty() const {
 return top == -1;
 }
-void push(T item) {
-if (top < MSSize - 1)
+void push(const T& item) {
+if (top < MSSize - 1) {
 arr[++top] = item;
+} else {
+throw std::overflow_error("Stack overflow");
+}
 }
 void pop() {
-if (top >= 0)
+if (top >= 0) {
 top--;
+} else {
+throw std::underflow_error("Stack underflow");
+}
 }
 };
 #endif  // INCLUDE_TSTACK_H_
