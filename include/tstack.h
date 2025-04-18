6b9ac896a3eea11710c8147ddd6ec127ebec7e97
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-18 18:47:13 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,20 +2,20 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 #include <vector>
-
-template<typename T, int MSSize>
+#include <stdexept>
+template<typename T, int kStackSize>
 class TStack {
  private:
 std::vector<T> data;
 int top;
 
  public:
-TStack() : top(-1) {}
+TStack() : top(-1), data(kStackSize) {}
 bool isEmpty() const {
 return top == -1;
 }
 bool isFull() const {
-return top == MSSize - 1;
+return top == kStackSize - 1;
 }
 void push(const T& value) {
 if (!isFull()) {
