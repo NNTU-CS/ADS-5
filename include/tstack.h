f72aa0d861a0b551d34cee359f2c921a9cd0df52
Arseny <grigoriyevvv2@mail.ru>
2025-04-18 19:49:17 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T arr[size];
+    int top;
+  public:
+    Tstack():top(-1) {}
+  bool isEmpty() const {
+    return top == -1;
+  }
+  bool isFull() const {
+    return top == size - 1;
+  }
+  T get() const {
+    if(!isEmpty())
+      return arr[top];
+    else
+      throw std::string("Empty");
+  }
+  void push(const T&item) {
+    if(!isFull())
+      arr[++top] = item;
+    else
+      throw std::string("Full");
+  }
+  void pop() {
+    if(!isEmpty())
+      top--;
+    else
+      throw std::string("Empty");
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
