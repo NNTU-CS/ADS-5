5644d9ee3fb5b1618d009f0681376efbd9a2279f
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-04-18 22:38:56 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,35 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+#include <stdexcept>
+
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[size];
+  int top;
+
+ public:
+  TStack() : top(-1) {}
+
+  void push(const T& val) {
+    if (top >= size - 1) throw std::overflow_error("Stack overflow");
+    data[++top] = val;
+  }
+
+  T pop() {
+    if (top < 0) throw std::underflow_error("Stack underflow");
+    return data[top--];
+  }
+
+  T peek() const {
+    if (top < 0) throw std::underflow_error("Stack is empty");
+    return data[top];
+  }
+
+  bool isEmpty() const { return top == -1; }
+
+  bool isFull() const { return top == size - 1; }
 };
 
 #endif  // INCLUDE_TSTACK_H_
