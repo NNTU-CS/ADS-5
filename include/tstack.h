b465c728aed60300dbad5e84263cc0afc7f8669b
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-18 13:57:23 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,54 +1,30 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#include <stdexcept>
 
 template<typename T, int MSSize>
 class TStack {
  private:
-T data_[MSSize];
-size_t size_ = 0;
-
+T arr[MSSize];
+int top;
  public:
-T get(size_t index) const {
-if (index < size_) {
-return data_[index];
-} else {
-throw std::out_of_range("Index out of range");
+TStack() :top(-1) {}
+T get() const {
+return arr[top];
 }
+bool isFull() const {
+return top == MSSize - 1;
 }
-};
-
-bool empty() const { return size_ == 0; }
-size_t size() const { return size_; }
-
-void push(const T& value) {
-if (size_ == MSSize) {
-throw std::out_of_range("Stack overflow");
+bool isEmpty() const {
+return top == -1;
 }
-data_[size_++] = value;
+void push(T item) {
+if (top < MSSize - 1)
+arr[++top] = item;
 }
-
-T& top() {
-if (empty()) {
-throw std::out_of_range("Stack is empty");
-}
-return data_[size_ - 1];
-}
-
-const T& top() const {
-if (empty()) {
-throw std::out_of_range("Stack is empty");
-}
-return data_[size_ - 1];
-}
-
 void pop() {
-if (empty()) {
-throw std::out_of_range("Stack is empty");
-}
---size_;
+if (top >= 0)
+top--;
 }
 };
-
 #endif  // INCLUDE_TSTACK_H_
