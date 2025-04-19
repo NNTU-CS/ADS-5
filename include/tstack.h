9f752ac8b769cb7471b59f21fa46bdf664c866fb
ratatyq <n.mantorov@yandex.ru>
2025-04-20 00:58:45 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,33 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#ifndef GUARD_TSTACK_H
+#define GUARD_TSTACK_H
 
-template<typename T, int size>
+template <typename T, int max_depth>
 class TStack {
-  // добавьте код стека
+private:
+  T elements[max_depth];
+  int head_pos;
+public:
+  TStack() : head_pos(-1) {}
+
+  void push(const T& item) {
+    if (head_pos >= max_depth - 1) throw std::overflow_error("Stack full");
+    elements[++head_pos] = item;
+  }
+
+  T remove() {
+    if (head_pos < 0) throw std::underflow_error("Stack empty");
+    return elements[head_pos--];
+  }
+
+  T check() const {
+    if (head_pos < 0) throw std::underflow_error("No elements");
+    return elements[head_pos];
+  }
+
+  bool hasElements() const {
+    return head_pos != -1;
+  }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif  // GUARD_TSTACK_H
