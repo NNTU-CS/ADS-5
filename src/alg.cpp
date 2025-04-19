07bcf52fbdafcab3cfd01715b1c9d4405e5f7889
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-04-19 19:41:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,62 @@
 #include "tstack.h"
 
 std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+    std::string result;
+    TStack<char, 100> stack;
+    std::map<char, int> precedence = {{'*', 3}, {'/', 3}, {'+', 2}, {'-', 2}, {'(', 1}};
+    for (char c : inf) {
+        if (isdigit(c)) {
+            result += c;
+        } else if (c == '(') {
+            stack.push(c);
+        } else if (c == ')') {
+            while (!stack.isEmpty() && stack.top() != '(') {
+                result += ' ';
+                result += stack.pop();
+            }
+            stack.pop();
+        } else {
+            if (!result.empty() && isdigit(result.back())) {
+                result += ' ';
+            }
+            while (!stack.isEmpty() && precedence[stack.top()] >= precedence[c]) {
+                result += stack.pop();
+                result += ' ';
+            }
+            stack.push(c);
+        }
+    }
+    while (!stack.isEmpty()) {
+        result += ' ';
+        result += stack.pop();
+    }
+    return result;
 }
 
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+    TStack<int, 100> stack;
+    int num = 0;
+    bool readingNum = false;
+    for (char c : post) {
+        if (isdigit(c)) {
+            num = num * 10 + (c - '0');
+            readingNum = true;
+        } else if (c == ' ') {
+            if (readingNum) {
+                stack.push(num);
+                num = 0;
+                readingNum = false;
+            }
+        } else {
+            int b = stack.pop();
+            int a = stack.pop();
+            switch (c) {
+                case '+': stack.push(a + b); break;
+                case '-': stack.push(a - b); break;
+                case '*': stack.push(a * b); break;
+                case '/': stack.push(a / b); break;
+            }
+        }
+    }
+    return stack.top();
 }
