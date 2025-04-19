a165f9ec780d88c7412b0fa498b9b68ce7a89248
yeefip <yeefipp@gmail.com>
2025-04-19 13:49:22 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,36 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+template<typename T, int size>
+class TStack {
+ private:
+  T data[size] = {};
+  int top = -1;
+
+ public:
+  void push(const T& val) { 
+    if (top < size - 1) {
+      data[++top] = val;
+    }
+  }
+
+  T pop() { 
+    if (top >= 0) {
+      return data[top--];
+    }
+    return T();
+  }
+
+  T topElem() const { 
+    if (top >= 0) {
+      return data[top];
+    }
+    return T();
+  }
+
+  bool isEmpty() const { 
+    return top == -1; 
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
