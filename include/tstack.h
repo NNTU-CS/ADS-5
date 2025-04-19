fab31952215e626e3b0bcde41af5b0f0a6eed7cd
Анна <sol0dova.anna@yandex.ru>
2025-04-19 13:28:49 +0300

    ads-5

--- include/tstack.h
+++ include/tstack.h
@@ -7,27 +7,21 @@ class TStack {
  private:
     T data[size];
     int top;
-
  public:
     TStack() : top(-1) {}
     void push(const T& value) {
-        if (top >= size - 1)
-            throw "Stack overflow";
+        if (top >= size - 1) throw "Stack overflow";
         data[++top] = value;
     }
     T pop() {
-        if (isEmpty())
-            throw "Stack underflow";
+        if (isEmpty()) throw "Stack underflow";
         return data[top--];
     }
     T peek() const {
-        if (isEmpty())
-            throw "Stack is empty";
+        if (isEmpty()) throw "Stack is empty";
         return data[top];
     }
-    bool isEmpty() const {
-        return top == -1;
-    }
+    bool isEmpty() const { return top == -1; }
 };
 
 #endif  // INCLUDE_TSTACK_H_
