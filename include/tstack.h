82ec8c732c66ab98dc2dc8e88f959b81a4991f6a
yeefip <yeefipp@gmail.com>
2025-04-19 14:11:37 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,35 +1,35 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
  private:
-  T data[size] = {};
+  T data[size] = {};  // Удалены лишние пробелы в конце строки
   int top = -1;
 
  public:
-  void push(const T& val) { 
+  void push(const T& val) {
     if (top < size - 1) {
       data[++top] = val;
     }
   }
 
-  T pop() { 
+  T pop() {
     if (top >= 0) {
       return data[top--];
     }
     return T();
   }
 
-  T topElem() const { 
+  T topElem() const {
     if (top >= 0) {
       return data[top];
     }
     return T();
   }
 
-  bool isEmpty() const { 
+  bool isEmpty() const {
     return top == -1;
   }
 };
