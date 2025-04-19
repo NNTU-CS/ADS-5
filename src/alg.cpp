11b527a0b8e1f13b4816663bbf99399195301031
yeefip <yeefipp@gmail.com>
2025-04-19 13:29:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,35 @@
 // Copyright 2025 NNTU-CS
 #include <string>
-#include <map>
 #include <cctype>
-#include "tstack.h"
+
+template <typename T, int size>
+class TStack {
+ private:
+  T arr[size];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  void push(const T& value) {
+    if (topIndex >= size - 1) throw "Stack overflow";
+    arr[++topIndex] = value;
+  }
+
+  T pop() {
+    if (topIndex < 0) throw "Stack underflow";
+    return arr[topIndex--];
+  }
+
+  T top() const {
+    if (topIndex < 0) throw "Stack is empty";
+    return arr[topIndex];
+  }
+
+  bool isEmpty() const {
+    return topIndex < 0;
+  }
+};
 
 int priority(char op) {
   if (op == '+' || op == '-') return 1;
@@ -10,30 +37,23 @@ int priority(char op) {
   return 0;
 }
 
-std::string infx2pstfx(const std::string& inf) {
-  TStack<char, 100> opStack;
+std::string infx2pstfx(const std::string& infx) {
   std::string output;
-  for (size_t i = 0; i < inf.length(); ++i) {
-    char c = inf[i];
-    if (std::isdigit(c)) {
-      // читаем полное число
-      while (i < inf.length() && std::isdigit(inf[i])) {
-        output += inf[i++];
-      }
-      output += ' ';
-      --i;
+  TStack<char, 100> opStack;
+
+  for (char c : infx) {
+    if (isdigit(c)) {
+      output += c;
     } else if (c == '(') {
       opStack.push(c);
     } else if (c == ')') {
       while (!opStack.isEmpty() && opStack.top() != '(') {
         output += opStack.pop();
-        output += ' ';
       }
-      if (!opStack.isEmpty()) opStack.pop();
-    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+      if (!opStack.isEmpty()) opStack.pop();  // Удаляем '('
+    } else {
       while (!opStack.isEmpty() && priority(opStack.top()) >= priority(c)) {
         output += opStack.pop();
-        output += ' ';
       }
       opStack.push(c);
     }
@@ -41,24 +61,18 @@ std::string infx2pstfx(const std::string& inf) {
 
   while (!opStack.isEmpty()) {
     output += opStack.pop();
-    output += ' ';
   }
 
   return output;
 }
 
-int eval(const std::string& post) {
+int eval(const std::string& pstfx) {
   TStack<int, 100> valStack;
-  for (size_t i = 0; i < post.length(); ++i) {
-    char c = post[i];
-    if (std::isdigit(c)) {
-      int num = 0;
-      while (i < post.length() && std::isdigit(post[i])) {
-        num = num * 10 + (post[i++] - '0');
-      }
-      valStack.push(num);
-      --i;
-    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+
+  for (char c : pstfx) {
+    if (isdigit(c)) {
+      valStack.push(c - '0');
+    } else {
       int b = valStack.pop();
       int a = valStack.pop();
       switch (c) {
@@ -69,5 +83,6 @@ int eval(const std::string& post) {
       }
     }
   }
+
   return valStack.pop();
 }
