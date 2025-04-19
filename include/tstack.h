f45dacf22287ecedc07222cf080eb649285871d0
perchig <ivanslepko11@gmail.com>
2025-04-19 22:46:22 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+T* array;
+int top;
+
+public:
+TStack(): array(new T[size]), top(-1) {}
+~TStack() {
+  delete[] array;
+}
+bool is_empty() const {
+    return top == -1;
+}
+bool is_full() const {
+    return top == ( size - 1 );
+}
+void push(const T& n) {
+    if (!is_full())
+    array[++top] = n;
+}
+void pop() {
+    if (!is_empty())
+      top--;
+}
+T peek() const {
+    if (!is_empty())
+      return array[top];
+    else
+      return T();
+}
 };
 
 #endif  // INCLUDE_TSTACK_H_
