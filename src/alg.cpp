aa87a8666df74f265fdc466e7cd167206d1aa4be
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-04-20 20:26:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,8 +4,52 @@
 #include "tstack.h"
 
 std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+  std::string postfix;
+  TStack<char, 100> opStack;
+  for (int i = 0; i < inf.length(); i++) {
+    char temp = inf[i];
+    if (isdigit(temp)) {
+      std::string temp_string;
+      while (i < inf.length() && isdigit(inf[i])) {
+        temp_string += temp;
+        i++;
+        temp = inf[i];
+      }
+      i--;
+      postfix += temp_string;
+      postfix += " ";
+    } else if (temp == '(') {
+      opStack.push(temp);
+    } else if (temp == ')') {
+      while (!opStack.isEmpty() && opStack.get() != '(') {
+        postfix += opStack.get();
+        postfix += " ";
+        opStack.pop();
+      }
+      if (!opStack.isEmpty() && opStack.get() == '(') {
+        opStack.pop();
+      }
+    } else if (temp == '+' || temp == '-' || temp == '*' || temp == '/') {
+      while (!opStack.isEmpty() && checkpriority(temp) <=
+        checkpriority(opStack.get())) {
+        postfix += opStack.get();
+        postfix += " ";
+        opStack.pop();
+      }
+      opStack.push(temp);
+    } else if (temp == ' '){
+      continue;
+    }
+  }
+  while (!opStack.isEmpty()) {
+    postfix += opStack.get();
+    postfix += " ";
+    opStack.pop();
+  }
+  if (!postfix.empty() && postfix.back() == ' ') {
+    postfix.pop_back();
+  }
+  return postfix;
 }
 
 int eval(const std::string& pref) {
