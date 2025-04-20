88850a1ad9e7afc69833fa63f10363ab9216a12d
n1kve <nikita06vet@gmail.com>
2025-04-20 16:05:55 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,35 +4,41 @@
 
 template<typename T, int size>
 class TStack {
-private:
+ private:
     T el[size];
     int indx;
-public:
+
+ public:
     TStack() : indx(-1) {}
+
     bool push(const T& value) {
         if (indx >= size - 1) {
-            return false; // Стек переполнен
+            return false;
         }
         el[++indx] = value;
         return true;
     }
+
     bool pop(T& value) {
         if (indx < 0) {
-            return false; // Стек пуст
+            return false;
         }
         value = el[indx--];
         return true;
     }
+
     bool top(T& value) const {
         if (indx < 0) {
-            return false; // Стек пуст
+            return false;
         }
         value = el[indx];
         return true;
     }
+
     bool isEmpty() const {
         return indx == -1;
     }
+
     int getSize() const {
         return indx + 1;
     }
