251555924a09c21ed1a71c56957f1a63c515b9c5
Burkov06 <s.burkov.2006@gmail.com>
2025-04-20 20:22:26 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,53 +1,10 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#ifndef INCLUDE_ALG_H_
+#define INCLUDE_ALG_H_
 
-#include <iostream>
+#include <string>
 
-template<typename T, int kStackSize>
-class TStack {
- private:
-  T data[kStackSize];
-  int topIndex;
+std::string infx2pstfx(std::string);
+int eval(std::string);
 
- public:
-  TStack() : topIndex(-1) {}
-
-  bool isEmpty() const {
-    return topIndex == -1;
-  }
-
-  bool isFull() const {
-    return topIndex == kStackSize - 1;
-  }
-
-  void push(const T& element) {
-    if (isFull()) {
-      std::cerr << "Stack Overflow!" << std::endl;
-      return;
-    }
-    data[++topIndex] = element;
-  }
-
-  T pop() {
-    if (isEmpty()) {
-      std::cerr << "Stack Underflow!" << std::endl;
-      return T();
-    }
-    return data[topIndex--];
-  }
-
-  T top() const {
-    if (isEmpty()) {
-      std::cerr << "Stack is Empty!" << std::endl;
-      return T();
-    }
-    return data[topIndex];
-  }
-
-  int get_size() const {
-    return kStackSize;
-  }
-};
-
-#endif  // INCLUDE_TSTACK_H_
+#endif  // INCLUDE_ALG_H_
