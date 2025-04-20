30359abac1e793f0784e13855b120955739895c0
jafar khadour <jkhadour111@gmail.com>
2025-04-20 16:21:53 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,33 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int Size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[Size];
+  int top_index;
+
+ public:
+  TStack() : top_index(-1) {}
+
+  void push(const T& value) {
+    if (top_index >= Size - 1) throw std::overflow_error("Stack full");
+    data[++top_index] = value;
+  }
+
+  T remove() {
+    if (top_index < 0) throw std::underflow_error("Stack empty");
+    return data[top_index--];
+  }
+
+  T check() const {
+    if (top_index < 0) throw std::underflow_error("No elements");
+    return data[top_index];
+  }
+
+  bool hasElements() const {
+    return top_index >= 0;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
