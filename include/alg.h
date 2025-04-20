2ed59ab6808e6c8c263c88043e62b6aa691b1b1a
Burkov06 <s.burkov.2006@gmail.com>
2025-04-20 20:08:38 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,10 +1,51 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_ALG_H_
-#define INCLUDE_ALG_H_
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
-#include <string>
+template<typename T, int size>
+class TStack {
+ private:
+  T data[size];
+  int topIndex;
 
-std::string infx2pstfx(std::string);
-int eval(std::string);
+ public:
+  TStack() : topIndex(-1) {}
 
-#endif  // INCLUDE_ALG_H_
+  bool isEmpty() const {
+    return topIndex == -1;
+  }
+
+  bool isFull() const {
+    return topIndex == size - 1;
+  }
+
+  void push(const T& element) {
+    if (isFull()) {
+      std::cerr << "Stack Overflow!" << std::endl;
+      return;
+    }
+    data[++topIndex] = element;
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      std::cerr << "Stack Underflow!" << std::endl;
+      return T();
+    }
+    return data[topIndex--];
+  }
+
+  T top() const {
+    if (isEmpty()) {
+      std::cerr << "Stack is Empty!" << std::endl;
+      return T();
+    }
+    return data[topIndex];
+  }
+
+  int get_size() const {
+    return size;
+  }
+};
+
+#endif  // INCLUDE_TSTACK_H_
