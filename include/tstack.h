be326b51a3e87ec73c316b9ada711c27ce4ad4da
NastyaIlina07 <149273255+NastyaIlina07@users.noreply.github.com>
2025-04-20 17:13:57 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,36 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  static const int max_size = size;
+  T arr[max_size];
+  int top;
+public:
+  TStack() : top(-1) {}
+  bool isEmpty() const {
+    return top == -1;
+  }
+  bool isFull() const {
+    return top == max_size - 1;
+  }
+  T getTop() const {
+    if (isEmpty()) {
+      throw std::out_of_range("Stack is emty");
+    } return arr[top];
+  }
+  void push(const T& value) {
+    if (isFull()) {
+      throw std::overflow_error("Stack is full");
+    } arr[++top] = value;
+  }
+  void pop() {
+    if (isEmpty()) {
+      throw std::underflow_error("Stack is empty");
+    } --top;
+  }
+  int size() const {
+    return top + 1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
