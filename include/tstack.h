5eda62724be410d9d392ebbb8272de7826240d4a
GLUKERZZZ <artem2801200635@gmail.com>
2025-04-20 20:50:15 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,37 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+public:
+    TStack() : top(-1) {}
+
+    void push(const T& item) {
+        if (top >= size - 1) {
+            throw std::overflow_error("Stack overflow");
+        }
+        data[++top] = item;
+    }
+
+    void pop() {
+        if (top < 0) {
+            throw std::underflow_error("Stack underflow");
+        }
+        --top;
+    }
+
+    T topItem() const {
+        if (top < 0) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[top];
+    }
+
+    bool isEmpty() const {
+        return top == -1;
+    }
+
+private:
+    T data[size];
+    int top;
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
