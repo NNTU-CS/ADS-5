d20ecc105ace9f46402cb4d75c55c8a21cd7dc67
t7cel <bachinalva@gmail.com>
2025-04-20 10:42:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,42 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[K];
+    int topInd;
+
+ public:
+    TStack() : topInd(-1) {}
+    bool isEmpty() const {
+        return topInd < 0;
+    }
+    bool isFull() const {
+        return topInd >= K - 1;
+    }
+    void push(const T& item) {
+        if (isFull()) {
+            throw std::runtime_error("Full");
+        }
+        data[++topInd] = item;
+    }
+    void pop() {
+        if (isEmpty()) {
+            throw std::runtime_error("Empty");
+        }
+        --topInd;
+    }
+    T& top() {
+        if (isEmpty()) {
+            throw std::runtime_error("Empty");
+    }
+        return data[topInd];
+    }
+    const T& top() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Empty");
+        }
+        return data[topInd];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
