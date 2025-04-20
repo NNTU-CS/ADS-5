0435bf2b73b0b11be246c6aded070b484a946044
NastyaIlina07 <149273255+NastyaIlina07@users.noreply.github.com>
2025-04-20 18:23:16 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,39 +1,34 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <array>
+#include <stdexcept>
 
 template<typename T, int size>
 class TStack {
 private:
-  static const int max_size = size;
-  T arr[max_size];
-  int top;
+  static constexpr int kStackSize = size;  
+  std::array<T, kStackSize> data_;           
+  int top_{ -1 };
 public:
-  TStack() : top(-1) {}
-  bool isEmpty() const {
-    return top == -1;
-  }
-  bool isFull() const {
-    return top == max_size - 1;
-  }
-  T getTop() const {
-    if (isEmpty()) {
-      throw std::out_of_range("Stack is emty");
-    } return arr[top];
-  }
   void push(const T& value) {
-    if (isFull()) {
-      throw std::overflow_error("Stack is full");
-    } arr[++top] = value;
+    if (full()) throw std::overflow_error("stack overflow");
+    data_[++top_] = value;
   }
   void pop() {
-    if (isEmpty()) {
-      throw std::underflow_error("Stack is empty");
-    } --top;
+    if (empty()) throw std::underflow_error("stack underflow");
+    --top_;
   }
-  int size() const {
-    return top + 1;
+  T& top() {
+    if (empty()) throw std::underflow_error("stack empty");
+    return data_[top_];
   }
+  const T& top() const {
+    if (empty()) throw std::underflow_error("stack empty");
+    return data_[top_];
+  }
+  int size()  const { return top_ + 1; }          
+  bool empty() const { return top_ < 0; }
+  bool full()  const { return top_ == kStackSize - 1; }
 };
-
 #endif  // INCLUDE_TSTACK_H_
