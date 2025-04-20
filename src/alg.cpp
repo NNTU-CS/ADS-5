dab37e12127c3183222e41cf17f02550896f6f70
slavangg52 <slavatop0203@gmail.com>
2025-04-21 00:01:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,124 +1,105 @@
 // Copyright 2025 NNTU-CS
 #include <string>
-#include <map>
+#include <sstream>
+#include <stack>
+#include <cctype>
+#include <algorithm>
 #include "tstack.h"
 
-bool isDigit(char ch) {
-    return (ch >= '0' && ch <= '9');
+static int getPrecedence(char oper) {
+    return (oper == '+' || oper == '-') ? 1 :
+           (oper == '*' || oper == '/') ? 2 : 0;
 }
 
-bool isOperator(char ch) {
-    return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
+static bool isMathOperator(char ch) {
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/';
 }
 
-int Prioritet(char op) {
-    switch (op) {
-    case '(':
-    case ')':
-        return 0;
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '/':
-        return 2;
-    default:
-        return -1;
-    }
-}
+std::string infx2pstfx(const std::string& infixExpr) {
+    std::stack<char> operatorStack;
+    std::string postfixExpr;
 
-std::string infx2pstfx(const std::string& infix) {
-    if (infix.empty()) {
-        throw "Is empty";
-    }
+    for (size_t idx = 0; idx < infixExpr.size(); ++idx) {
+        char currentChar = infixExpr[idx];
 
-    std::string postfix;
-    TStack<char, 50> operators;
-
-    for (size_t i = 0; i < infix.length(); ++i) {
-        char ch = infix[i];
-
-        if (isDigit(ch)) {
+        if (std::isdigit(currentChar)) {
             std::string number;
-            number += ch;
-
-            while (i + 1 < infix.length() && isDigit(infix[i + 1])) {
-                number += infix[++i];
+            while (idx < infixExpr.size() &&
+                   std::isdigit(infixExpr[idx])) {
+                number.push_back(infixExpr[idx++]);
             }
-
-            postfix += number;
-            postfix += ' ';
+            --idx;
+            postfixExpr += number;
+            postfixExpr.push_back(' ');
         }
-        else if (ch == '(') {
-            operators.Push(ch);
+        else if (currentChar == '(') {
+            operatorStack.push(currentChar);
         }
-        else if (ch == ')') {
-            while (!operators.isEmpty() && operators.peekLast() != '(') {
-                postfix += operators.pop();
-                postfix += ' ';
+        else if (currentChar == ')') {
+            while (!operatorStack.empty() &&
+                   operatorStack.top() != '(') {
+                postfixExpr.push_back(operatorStack.top());
+                postfixExpr.push_back(' ');
+                operatorStack.pop();
             }
-            if (!operators.isEmpty()) {
-                operators.pop();
+            if (!operatorStack.empty()) {
+                operatorStack.pop(); // удалить '('
             }
         }
-        else if (isOperator(ch)) {
-            while (!operators.isEmpty() &&
-                   Prioritet(ch) <= Prioritet(operators.peekLast())) {
-                postfix += operators.pop();
-                postfix += ' ';
+        else if (isMathOperator(currentChar)) {
+            while (!operatorStack.empty() &&
+                   isMathOperator(operatorStack.top()) &&
+                   getPrecedence(operatorStack.top()) >=
+                   getPrecedence(currentChar)) {
+                postfixExpr.push_back(operatorStack.top());
+                postfixExpr.push_back(' ');
+                operatorStack.pop();
             }
-            operators.Push(ch);
+            operatorStack.push(currentChar);
         }
     }
 
-    while (!operators.isEmpty()) {
-        postfix += operators.pop();
-        postfix += ' ';
+    while (!operatorStack.empty()) {
+        if (operatorStack.top() != '(') {
+            postfixExpr.push_back(operatorStack.top());
+            postfixExpr.push_back(' ');
+        }
+        operatorStack.pop();
     }
 
-    if (!postfix.empty()) {
-        postfix.pop_back();
+    if (!postfixExpr.empty() && postfixExpr.back() == ' ') {
+        postfixExpr.pop_back();
     }
 
-    return postfix;
+    return postfixExpr;
 }
 
-int eval(const std::string& postfix) {
-    TStack<int, 50> operands;
+int eval(const std::string& postfixExpr) {
+    std::stack<int> valueStack;
+    std::istringstream exprStream(postfixExpr);
+    std::string token;
 
-    for (size_t i = 0; i < postfix.length(); ++i) {
-        char ch = postfix[i];
+    while (exprStream >> token) {
+        if (token.size() == 1 && isMathOperator(token[0])) {
+            int rightOperand = valueStack.top();
+            valueStack.pop();
+            int leftOperand = valueStack.top();
+            valueStack.pop();
 
-        if (isDigit(ch)) {
-            std::string number;
-            number += ch;
+            char op = token[0];
+            int result = 0;
 
-            while (i + 1 < postfix.length() && isDigit(postfix[i + 1])) {
-                number += postfix[++i];
-            }
+            if (op == '+') result = leftOperand + rightOperand;
+            else if (op == '-') result = leftOperand - rightOperand;
+            else if (op == '*') result = leftOperand * rightOperand;
+            else if (op == '/') result = leftOperand / rightOperand;
 
-            operands.Push(std::stoi(number));
+            valueStack.push(result);
         }
-        else if (isOperator(ch)) {
-            int right = operands.pop();
-            int left  = operands.pop();
-
-            switch (ch) {
-            case '+':
-                operands.Push(left + right);
-                break;
-            case '-':
-                operands.Push(left - right);
-                break;
-            case '*':
-                operands.Push(left * right);
-                break;
-            case '/':
-                operands.Push(left / right);
-                break;
-            }
+        else {
+            valueStack.push(std::stoi(token));
         }
     }
 
-    return operands.pop();
+    return valueStack.top();
 }
