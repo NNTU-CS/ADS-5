dd6b7f1c2701a0b210b33627d48bfd746a2eb838
YaroslavChinyakov <chinyakov777@gmail.com>
2025-04-20 18:31:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -29,36 +29,36 @@ std::string infx2pstfx(const std::string& inf) {
             output.push_back(' ');
         }
         else if (c == '(') {
-            st.push(c);
+            st.Push(c);
         }
         else if (c == ')') {
-            while (!st.empty() && st.top() != '(') {
+            while (!st.IsEmpty() && st.Top() != '(') {
                 output.push_back(st.top());
                 output.push_back(' ');
-                st.pop();
+                st.Pop();
             }
             if (!st.empty() && st.top() == '(') {
-                st.pop();
+                st.Pop();
             }
         }
         else if (isOperator(c)) {
-            while (!st.empty() && isOperator(st.top()) &&
+            while (!st.IsEmpty() && isOperator(st.Top()) &&
                 precedence(st.top()) >= precedence(c)) {
-                output.push_back(st.top());
+                output.push_back(st.Top());
                 output.push_back(' ');
-                st.pop();
+                st.Pop();
             }
-            st.push(c);
+            st.Push(c);
         }
         else {
         }
     }
-    while (!st.empty()) {
-        if (st.top() != '(') {
-            output.push_back(st.top());
+    while (!st.IsEmpty()) {
+        if (st.Top() != '(') {
+            output.push_back(st.Top());
             output.push_back(' ');
         }
-        st.pop();
+        st.Pop();
     }
     if (!output.empty() && output.back() == ' ') {
         output.pop_back();
@@ -73,8 +73,8 @@ int eval(const std::string& post) {
     while (iss >> token) {
         char c = token[0];
         if (token.size() == 1 && isOperator(c)) {
-            int b = st.top(); st.pop();
-            int a = st.top(); st.pop();
+            int b = st.Top(); st.Pop();
+            int a = st.Top(); st.Pop();
             int res = 0;
             if (c == '+') {
                 res = a + b;
@@ -88,11 +88,11 @@ int eval(const std::string& post) {
             else if (c == '/') {
                 res = a / b;
             }
-            st.push(res);
+            st.Push(res);
         }
         else {
-            st.push(std::stoi(token));
+            st.Push(std::stoi(token));
         }
     }
-    return st.top();
+    return st.Top();
 }
