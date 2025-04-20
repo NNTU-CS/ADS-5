11587fedc2d3ac05ea2a734a625b7f02fdf3cc0b
HntgerL <lbedevd@gmail.com>
2025-04-20 23:41:00 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,11 +4,9 @@
 
 template<typename T, int size>
 class TStack {
- 
  private:
-    std::array<T, size>
+    T arr[size];
     int top;
- 
  public:
     TStack(): top(-1) {}
     bool isEmpty()const {
