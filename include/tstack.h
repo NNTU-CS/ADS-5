5b119ed29c772edcbe85ac7eefdad79ec3bde510
tilt377-3 <wardroiddarkness@gmail.com>
2025-04-21 00:10:25 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -7,27 +7,21 @@ class TStack {
 private:
   T data[size] = {};
   int top = -1;
-
 public:
   TStack() : top(-1) {}
-
   void push(const T& value) {
     if (top < size - 1) {
       data[++top] = value;
     }
   }
-
   T pop() {
     return top >= 0 ? data[top--] : T();
   }
-
   T Top() const {
     return top >= 0 ? data[top] : T();
   }
-
   bool empty() const {
     return top == -1;
   }
 };
-
 #endif  // INCLUDE_TSTACK_H_
