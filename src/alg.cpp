184ea33b6b9a924930c55735f0515fe11988749f
nastuy <nastuysandalova@gmail.com>
2025-04-20 15:07:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,99 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+bool isOperator(char c) {
+  return c == '+' || c == '-' || c == '*' || c == '/';
 }
-
+int getPriority(char op) {
+  switch (op) {
+  case '(':
+    return 0;
+  case '+': case '-':
+    return 1;
+  case '*': case '/':
+    return 2;
+  default:
+    return -1;
+  }
+}
+std::string infx2pstfx(const std::string& inf) {
+  TStack<char> stack(100);
+  std::string postfix;
+  bool flag = false;
+  for (size_t i = 0; i < inf.size(); ++i) {
+    char c = inf[i];
+    if (c >= '0' && c <= '9') {
+      if (flag) {
+        postfix += c;
+      } else {
+        if (!postfix.empty())
+          postfix += ' ';
+        postfix += c;
+      }
+      flag = true;
+    } else if (c == '(') {
+      stack.push(c);
+      flag = false;
+    } else if (c == ')') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        postfix += ' ';
+        postfix += stack.get();
+        stack.pop();
+      }
+      if (!stack.isEmpty()) stack.pop();
+      flag = false;
+    } else if (isOperator(c)) {
+      while (!stack.isEmpty() && getPriority(stack.get()) >= getPriority(c)) {
+        postfix += ' ';
+        postfix += stack.get();
+        stack.pop();
+      }
+      stack.push(c);
+      flag = false;
+    }
+  }
+  while (!stack.isEmpty()) {
+    postfix += ' ';
+    postfix += stack.get();
+    stack.pop();
+  }
+  return postfix;
+}
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+  TStack<int> stack(100);
+  int num = 0;
+  bool flag = false;
+  for (size_t i = 0; i < pref.size(); ++i) {
+    char c = pref[i];
+    if (c >= '0' && c <= '9') {
+      num = num * 10 + (c - '0');
+      flag = true;
+    } else if (c == ' ') {
+      if (flag) {
+        stack.push(num);
+        num = 0;
+        flag = false;
+      }
+    } else if (isOperator(c)) {
+      int op2 = stack.get();
+      stack.pop();
+      int op1 = stack.get();
+      stack.pop();
+      switch (c) {
+      case '+':
+        stack.push(op1 + op2);
+        break;
+      case '-':
+        stack.push(op1 - op2);
+        break;
+      case '*':
+        stack.push(op1 * op2);
+        break;
+      case '/':
+        stack.push(op1 / op2);
+        break;
+      }
+    }
+  }
+  return stack.get();
 }
