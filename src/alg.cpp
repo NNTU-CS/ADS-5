df4701426bfb52c80136f7eefd3526a3ed8825d0
HntgerL <lbedevd@gmail.com>
2025-04-20 23:17:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,23 +15,20 @@ int prioritet(char oper) {
     return 2;
   default:
     return 0;
- }
+  }
 }
 
 std::string infx2pstfx(const std::string& inf) {
   TStack<char, 100>stack;
   int len = 0;
-  int count_p = 0;
-  char str[100] = { 0 };
+  std::string pref;
   while (inf[len] != '\0') {
     len += 1;
   }
   for (int count = 0; count < len; count++) {
     if (inf[count] >= '0' && inf[count] <= '9') {
-      str[count_p] = inf[count];
-      count_p += 1;
-      str[count_p] = ' ';
-      count_p += 1;
+      pref += inf[count];
+      pref += ' ';
     } else if (inf[count] == '(') {
       stack.push(inf[count]);
     } else if (inf[count] == ')') {
@@ -47,25 +44,20 @@ std::string infx2pstfx(const std::string& inf) {
       }
     } else {
       while (!stack.isEmpty() && prioritet(stack.get()) >= prioritet(inf[count]) ) {
-        str[count_p] = stack.get();
-        count_p += 1;
-        str[count_p] = ' ';
-        count_p += 1;
+        pref += stack.get();
+        pref += ' ';
         stack.pop();
       }
       stack.push(inf[count]);
     }
   }
   while (!stack.isEmpty()) {
-    str[count_p] = stack.get();
-    count_p += 1;
+    pref += stack.get();
     stack.pop();
     if (!stack.isEmpty()) {
-      str[count_p] = ' ';
-      count_p += 1;
+      pref += ' ';
     }
   }
-  std::string pref = str;
   return pref;
 }
 
@@ -80,28 +72,28 @@ int eval(const std::string& pref) {
   for (int count = 0; count < len; count++) {
     if (pref[count] >= '0' && pref[count] <= '9') {
       stack.push(pref[count]-'0');
-    } else if (pref[count] == '+') {
+    } else if (pref[count] == '+' ) {
       int right = stack.get();
       stack.pop();
       int left = stack.get();
       stack.pop();
       int final = left + right;
-			stack.push(final);
-	  } else if (pref[count] == '-') {
+      stack.push(final);
+    } else if (pref[count] == '-' ) {
       int right = stack.get();
       stack.pop();
       int left = stack.get();
       stack.pop();
       int final = left - right;
       stack.push(final);
-    } else if (pref[count] == '*') {
+    } else if (pref[count] == '*' ) {
       int right = stack.get();
       stack.pop();
       int left = stack.get();
       stack.pop();
       int final = left * right;
       stack.push(final);
-    } else if (pref[count] == '/') {
+    } else if (pref[count] == '/' ) {
       int right = stack.get();
       stack.pop();
       int left = stack.get();
