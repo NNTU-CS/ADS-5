523ac5dca1d222271748e8a0b0b5e92945c0176e
nastyanebuch12 <145349015+nastyanebuch12@users.noreply.github.com>
2025-04-20 13:36:23 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,38 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  class TStack {
+ private:
+  static const int kSize = size;
+  T arr[kSize];
+  int top;
+
+ public:
+  TStack() : top(-1) {}
+
+  bool isEmpty() const {
+    return top == -1;
+  }
+
+  bool isFull() const {
+    return top == size - 1;
+  }
+
+  T getTop() const {
+    return arr[top];
+  }
+
+  void push(const T& value) {
+    if (!isFull()) {
+      arr[++top] = value;
+    }
+  }
+
+  void pop() {
+    if (!isEmpty()) {
+      --top;
+    }
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
