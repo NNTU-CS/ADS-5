011473602d2428ad02a70e60333f4652d6042bde
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-04-21 06:48:47 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,31 +3,31 @@
 #define INCLUDE_TSTACK_H_
 template<typename T, int size>
 class TStack {
-   private:
-    static const int kCapacity = size;
-    T data[kCapacity];
-    int topIndex;
-   public:
-    TStack() : topIndex(0) {}
-    void push(const T& value) {
-      if (topIndex < kCapacity) {
-        data[topIndex++] = value;
-      }
+ private:
+  static const int kCapacity = size;
+  T data[kCapacity];
+  int topIndex;
+ public:
+  TStack() : topIndex(0) {}
+  void push(const T& value) {
+    if (topIndex < kCapacity) {
+      data[topIndex++] = value;
     }
-    T pop() {
-      if (topIndex > 0) {
-        return data[--topIndex];
-      }
-      return T();
+  }
+  T pop() {
+    if (topIndex > 0) {
+      return data[--topIndex];
     }
-    T Top() const {
-      if (topIndex > 0) {
-        return data[topIndex - 1];
-      }
-      return T();
+    return T();
+  }
+  T Top() const {
+    if (topIndex > 0) {
+      return data[topIndex - 1];
     }
-    bool empty() const {
-      return topIndex == 0;
-    }
+    return T();
+  }
+  bool empty() const {
+    return topIndex == 0;
+  }
 };
 #endif  // INCLUDE_TSTACK_H_
