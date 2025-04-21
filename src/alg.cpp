fac03cf75a8ac28206ad8c7f9867bff0535ff25d
Andrey-Trubachnov <137098778+Andrey-Trubachnov@users.noreply.github.com>
2025-04-21 20:53:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,138 @@
 // Copyright 2025 NNTU-CS
+#include <cctype>
 #include <string>
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+template<typename T, int size>
+std::string TStack<T, size>::infx2pstfx(const std::string& inf) {
+  std::string currentNumber = "";
+  for (char c : inf) {
+    if (isdigit(c)) {
+      currentNumber += c;
+      continue;
+    }
+    else {
+      if (!currentNumber.empty()) {
+        res += currentNumber;
+        res += ' ';
+        currentNumber = "";
+      }
+    }
+
+    if (c == '(') {
+      stack += c;
+    }
+    else if (c == '*' || c == '/') {
+      bool fl = false;
+      if (!stack.empty()) {
+        while (stack.back() == '*' || stack.back() == '/') {
+          res += stack.back();
+          res += ' ';
+          stack.pop_back();
+          fl = true;
+        }
+      }
+
+      if (fl) {
+        if (stack.back() == '(') {
+          stack.pop_back();
+        }
+      }
+
+      stack += c;
+    }
+    else if (c == '-' || c == '+') {
+      bool fl = false;
+      if (!stack.empty()) {
+        while (stack.back() == '-' || stack.back() == '+' || stack.back() == '*' || stack.back() == '/') {
+          res += stack.back();
+          res += ' ';
+          stack.pop_back();
+          fl = true;
+        }
+      }
+
+      if (fl) {
+        if (stack.back() == '(') {
+          stack.pop_back();
+        }
+      }
+
+      stack += c;
+    }
+    else if (c == ')') {
+      while (stack.back() != '(' && !stack.empty()) {
+        res += stack.back();
+        res += ' ';
+        stack.pop_back();
+      }
+
+      if (stack.back() == '(') {
+        stack.pop_back();
+      }
+    }
+  }
+
+  if (!currentNumber.empty()) {
+    res += currentNumber;
+    res += ' ';
+  }
+
+  while (!stack.empty()) {
+    res += stack.back();
+    res += ' ';
+    stack.pop_back();
+  }
+
+  res.pop_back();
+
+  return res;
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+template<typename T, int size>
+int TStack<T, size>::eval(const std::string& pref) {
+  int stack[size];
+  int top = -1;
+
+  int i = 0;
+  while (i < pref.size()) {
+    if (pref[i] == ' ') {
+      i++;
+    }
+
+    if (pref[i] == '+' || pref[i] == '-' || pref[i] == '*' || pref[i] == '/') {
+      int b = stack[top--];
+      int a = stack[top--];
+
+      char symbol = pref[i];
+      if (symbol == '+') {
+        stack[++top] = a + b; 
+      }
+      else if (symbol == '-') {
+        stack[++top] = a - b; 
+      }
+      else if (symbol == '*') {
+        stack[++top] = a * b; 
+      }
+      else {
+        stack[++top] = a / b; 
+      }
+
+      i++;
+    }
+    else if (isdigit(pref[i])) {
+      int num = 0;
+      while (i < pref.size() && isdigit(pref[i])) {
+        num = num * 10 + (pref[i] - '0');
+        i++;
+      }
+
+      stack[++top] = num;
+    }
+  }
+
+  return stack[top];
 }
+
+template class TStack<int, 100>;
