322c9537970027bae22a9be717c19531ab621d38
pootaatoovarenaya <lilechekT@mail.ru>
2025-04-21 18:42:37 +0300

    TTTTT.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,34 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+   private:
+    T data[size];
+    int topIndex;
+
+ public:
+    TStack() : topIndex(-1) {}
+    void push(T value) {
+      if (!isFull()) {
+        data[++topIndex] = value;
+      }
+    }
+T pop() {
+  if (!isEmpty()) {
+    return data[topIndex--];
+  }
+  return T();
+}
+T top() const {
+  if (!isEmpty()) {
+    return data[topIndex];
+  }
+  return T();
+}
+bool isEmpty() const {
+  return topIndex == -1;
+}
+bool isFull() const {
+  return topIndex == size - 1;
 };
 
 #endif  // INCLUDE_TSTACK_H_
