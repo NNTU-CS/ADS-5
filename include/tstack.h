4432338de347157364b4a67da02d00a283ceee0b
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-04-21 06:26:02 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,32 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T data[size];
+  int topIndex;
+public:
+  TStack() : topIndex(0) {}
+  void push(const T& value) {
+    if (topIndex < size) {
+      data[topIndex++] = value;
+    }
+  }
+  T pop() {
+    if (topIndex > 0) {
+      return data[--topIndex];
+    }
+    return T();
+  }
+  T Top() const {
+    if (topIndex > 0) {
+      return data[topIndex - 1];
+    }
+    return T();
+  }
+  bool empty() const {
+    return topIndex == 0;
+  }
 };
-
 #endif  // INCLUDE_TSTACK_H_
