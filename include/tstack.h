83e1b29a035f5b4799d4631bc79109383bab6d54
Lix <zelenovaaleksandra2@gmail.com>
2025-04-21 22:56:47 +0300

    Update lix

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,47 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#include <string>
+#include <array>
+#include <stdexcept>
 
-template<typename T, int size>
+#ifndef TSTACK_H
+#define TSTACK_H
+
+template <typename T, int SIZE>
 class TStack {
-  // добавьте код стека
+private:
+  T data[SIZE];
+  int top;
+
+public:
+  TStack() : top(-1) {}
+
+  bool isEmpty() const {
+    return top == -1;
+  }
+
+  bool isFull() const {
+    return top == SIZE - 1;
+  }
+
+  void push(const T& value) {
+    if (!isFull()) {
+      data[++top] = value;
+    }
+  }
+
+  T pop() {
+    if (!isEmpty()) {
+      return data[top--];
+    }
+    return T();
+  }
+
+  T peek() const {
+    if (!isEmpty()) {
+      return data[top];
+    }
+    return T();
+  }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
