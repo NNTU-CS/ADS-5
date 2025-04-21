c85d2673810e361699be30e56250402235ae2529
pivnik1 <niga.gurunov38@mail.ru>
2025-04-21 20:17:00 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T items[size];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  void push(const T& item) {
+    if (topIndex < size - 1) {
+      items[++topIndex] = item;
+    }
+  }
+
+  void pop() {
+    if (topIndex >= 0) {
+      --topIndex;
+    }
+  }
+
+  T top() const {
+    if (topIndex >= 0) {
+      return items[topIndex];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return topIndex < 0;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
