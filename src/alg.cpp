3e7a390acb6b7dbc0d67ef07043413d54f5cfc20
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-04-22 16:06:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <cctype>
 #include "tstack.h"
+// Copyright 2025 NNTU-CS
+#include <string>
+#include <cctype>
+#include "tstack.h"
 
 int precedence(char op) {
   if (op == '+' || op == '-') return 1;
@@ -12,17 +16,15 @@ int precedence(char op) {
 std::string infx2pstfx(const std::string& inf) {
   std::string post;
   TStack<char, 100> stack;
-
   for (size_t i = 0; i < inf.size(); ++i) {
     char c = inf[i];
     if (isspace(c)) continue;
-
     if (isdigit(c)) {
       while (i < inf.size() && isdigit(inf[i])) {
         post += inf[i++];
       }
       post += ' ';
-      i--;
+      --i;
     } else if (c == '(') {
       stack.push(c);
     } else if (c == ')') {
@@ -31,7 +33,7 @@ std::string infx2pstfx(const std::string& inf) {
         post += ' ';
         stack.pop();
       }
-      stack.pop();
+      if (!stack.empty()) stack.pop(); // Удалить '('
     } else {
       while (!stack.empty() && precedence(stack.top()) >= precedence(c)) {
         post += stack.top();
@@ -57,19 +59,16 @@ int eval(const std::string& post) {
   for (size_t i = 0; i < post.size(); ++i) {
     char c = post[i];
     if (isspace(c)) continue;
-
     if (isdigit(c)) {
       int num = 0;
       while (i < post.size() && isdigit(post[i])) {
         num = num * 10 + (post[i++] - '0');
       }
       stack.push(num);
-      i--;
+      --i;
     } else {
-      int val2 = stack.top();
-      stack.pop();
-      int val1 = stack.top();
-      stack.pop();
+      int val2 = stack.top(); stack.pop();
+      int val1 = stack.top(); stack.pop();
       switch (c) {
         case '+': stack.push(val1 + val2); break;
         case '-': stack.push(val1 - val2); break;
