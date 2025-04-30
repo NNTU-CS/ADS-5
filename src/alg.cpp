37a68eefa102443e7baf58e22bacb750a1aca4f1
icegergert-world <andrejkornev26@gmail.com>
2025-04-30 21:20:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,27 @@
+
 // Copyright 2025 NNTU-CS
 #include <string>
 #include <map>
 #include "tstack.h"
-
-std::map<char, int> op = {
+std::map<char, int> op_priority = {
     {'^', 4},
     {'*', 3}, {'/', 3},
     {'+', 2}, {'-', 2},
     {'(', 1}
 };
-
-bool isOperator(char c) {
-  return op.count(c) && c != '(';
-}
 bool isDigit(char c) {
   return c >= '0' && c <= '9';
 }
-int power(int base, int exp) {
-  int res = 1;
-  for (int i = 0; i < exp; ++i) {
-    res = res * base;
-  }
-  return res;
+bool isOperator(char c) {
+  return op_priority.count(c) && c != '(';
 }
-
+int power(int base, int exp) {
+  int result = 1;
+  for (int i = 0; i < exp; ++i) {
+    result *= base;
+  }
+  return result;
+}
 std::string infx2pstfx(const std::string& inf) {
   std::map<char, int> priority = {
       {'(', 0},
@@ -38,17 +36,17 @@ std::string infx2pstfx(const std::string& inf) {
     if (c == ' ') continue;
     if (isdigit(c)) {
       if (prevWasDigit) {
-        postfix = postfix + c;
+        postfix += c;
       } else {
         if (!postfix.empty() && postfix.back() != ' ') {
-          postfix = postfix + ' ';
+          postfix += ' ';
         }
-        postfix = postfix + c;
+        postfix += c;
       }
       prevWasDigit = true;
     } else {
       if (prevWasDigit) {
-        postfix = postfix + ' ';
+        postfix += ' ';
         prevWasDigit = false;
       }
       if (c == '(') {
@@ -56,20 +54,20 @@ std::string infx2pstfx(const std::string& inf) {
       } else if (c == ')') {
         while (!stack.empty() && stack.Top() != '(') {
           if (!postfix.empty() && postfix.back() != ' ') {
-            postfix = postfix + ' ';
+            postfix += ' ';
           }
-          postfix = postfix + stack.pop();
+          postfix += stack.pop();
         }
         stack.pop();
       } else {
         while (!stack.empty() && priority[stack.Top()] >= priority[c]) {
           if (!postfix.empty() && postfix.back() != ' ') {
-            postfix = postfix + ' ';
+            postfix += ' ';
           }
-          postfix = postfix + stack.pop();
+          postfix += stack.pop();
         }
         if (!postfix.empty() && postfix.back() != ' ') {
-          postfix = postfix + ' ';
+          postfix += ' ';
         }
         stack.push(c);
       }
@@ -77,46 +75,45 @@ std::string infx2pstfx(const std::string& inf) {
   }
   while (!stack.empty()) {
     if (!postfix.empty() && postfix.back() != ' ') {
-      postfix = postfix + ' ';
+      postfix += ' ';
     }
-    postfix = postfix + stack.pop();
+    postfix += stack.pop();
   }
   return postfix;
 }
-
 int eval(const std::string& pref) {
   TStack<int, 100> stack;
-  int n = 0;
+  int num = 0;
   bool readingNum = false;
   for (char c : pref) {
     if (c == ' ') {
       if (readingNum) {
-        stack.push(n);
-        n = 0;
+        stack.push(num);
+        num = 0;
         readingNum = false;
       }
       continue;
     }
     if (isdigit(c)) {
-      n = n * 10 + (c - '0');
+      num = num * 10 + (c - '0');
       readingNum = true;
     } else {
       if (readingNum) {
-        stack.push(n;
-        n = 0;
+        stack.push(num);
+        num = 0;
         readingNum = false;
       }
-      int x2 = stack.pop();
-      int x1 = stack.pop();
+      int b = stack.pop();
+      int a = stack.pop();
       switch (c) {
-      case '+': stack.push(x1 + x2); break;
-      case '-': stack.push(x1 - x2); break;
-      case '*': stack.push(x1 * x2); break;
-      case '/': stack.push(x1 / x2); break;
+      case '+': stack.push(a + b); break;
+      case '-': stack.push(a - b); break;
+      case '*': stack.push(a * b); break;
+      case '/': stack.push(a / b); break;
       case '^': {
-        int result = 1;
-        for (int i = 0; i < x2; ++i) result = result * x1;
-        stack.push(result);
+        int res = 1;
+        for (int i = 0; i < b; ++i) res *= a;
+        stack.push(res);
         break;
       }
       }
