cf7ff4417253ce954c21f960bfdf266e59ff094d
icegergert-world <andrejkornev26@gmail.com>
2025-04-30 20:51:17 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,45 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[kMaxSize];
+    int topIndex;
+
+ public:
+    TStack() : topIndex(-1) {}
+
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+
+    bool isFull() const {
+        return topIndex == kMaxSize - 1;
+    }
+
+    void push(const T& value) {
+        if (isFull()) {
+            throw std::overflow_error("Stack is full");
+        }
+        data[++topIndex] = value;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[topIndex--];
+    }
+
+    T top() const {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[topIndex];
+    }
+
+    int size() const {
+        return topIndex + 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
