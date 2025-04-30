08bd65528c04e61ab6127caa06999631e710f6c3
foltukol <foltukol@yandex.ru>
2025-04-30 09:35:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,22 @@
 #include <map>
 #include "tstack.h"
 
+bool is_digit(char c) {
+  return c >= '0' && c <= '9';
+}
+
+bool is_math_operator(char c) {
+  return (c == '+'  c == '-'  c == '*' || c == '/');
+}
+
+int operation_priority(char op) {
+  switch (op) {
+  case '+': case '-': return 1;
+  case '*': case '/': return 2;
+  default: return 0;
+  }
+}
+
 std::string infx2pstfx(const std::string& inf) {
   // добавьте код
   return std::string("");
