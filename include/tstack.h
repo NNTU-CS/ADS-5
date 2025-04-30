30e4e220e6a722377f17c918f5e232c1f3b9184d
icegergert-world <andrejkornev26@gmail.com>
2025-04-30 21:02:42 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,11 +4,11 @@
 
 template<typename T, int size>
 class TStack {
-private:
+ private:
     T data[kMaxSize];
     int topIndex;
 
- public:
+  public:
     TStack() : topIndex(-1) {}
 
     bool isEmpty() const {
