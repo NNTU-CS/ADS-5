61d692051bdf7eb7534cc6f70bfd3e591a6b1c5c
Kuchumova_Ekaterina <e_kuchumova_06@mail.ru>
2025-05-01 16:48:51 +0300

    tstack_2.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,29 +4,30 @@
 
 template<typename T, int size>
 class TStack {
-private:
-  T arr[size];
-  int top;
-public:
-  TStack() : top(-1) {}
-  void push(const T& val) {
-    if (top >= size - 1)
-      throw std::overflow_error("Stack overflow");
-    arr[++top] = val;
-  }
-  T pop() {
-    if (isEmpty())
-      throw std::underflow_error("Stack underflow");
-    return arr[top--];
-  }
-  T peek() const {
-    if (isEmpty())
-      throw std::underflow_error("Stack underflow");
-    return arr[top];
-  }
-  bool isEmpty() const {
-    return top == -1;
-  }
+  private:
+    static constexpr int kStackSize = size;
+    T arr[kStackSize];
+    int top;
+  public:
+    TStack() : top(-1) {}
+    void push(const T& val) {
+      if (top >= size - 1)
+        throw std::overflow_error("Stack overflow");
+      arr[++top] = val;
+    }
+    T pop() {
+      if (isEmpty())
+        throw std::underflow_error("Stack underflow");
+      return arr[top--];
+    }
+    T peek() const {
+      if (isEmpty())
+        throw std::underflow_error("Stack underflow");
+      return arr[top];
+    }
+    bool isEmpty() const {
+      return top == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
