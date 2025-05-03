749d6a5e90b38e376e7bb2e75f56639ab28021a2
liguhska <masharostenko@mail.ru>
2025-05-03 14:20:45 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -8,3 +8,27 @@ class TStack {
 };
 
 #endif  // INCLUDE_TSTACK_H_
+template <typename T, int SIZE>
+class TStack
+{
+private:
+    T data[SIZE];
+    int topIndex;
+
+public:
+    TStack(): topIndex(-1) {}
+    bool isEmpty() const { return topIndex == -1; }
+    bool isFull() const { return topIndex == SIZE - 1; }
+
+    void push(const T& value) {
+        if (!isFull()) data[++topIndex] = value;
+    }
+
+    T pop() {
+        return isEmpty() ? T() : data[topIndex--];
+    }
+
+    T peek() const {
+        return isEmpty() ? T() : data[topIndex];
+    }
+};
