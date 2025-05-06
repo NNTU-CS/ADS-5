7871569c29ed2d601131c72111135f01a30122dc
OlgaTimokhina <olya.tim0khina11@gmail.com>
2025-05-06 16:30:41 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,39 +1,40 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <stdexcept>
 
 template<typename T, int size>
 class TStack {
- public:
-  TStack() : topIndex_(-1) {}
-
-  void push(const T& value) {
-    if (isFull()) throw std::overflow_error("Stack overflow");
-    data_[++topIndex_] = value;
-  }
-
-  void pop() {
-    if (isEmpty()) throw std::underflow_error("Stack underflow");
-    --topIndex_;
-  }
-
-  T top() const {
-    if (isEmpty()) throw std::underflow_error("Stack is empty");
-    return data_[topIndex_];
-  }
-
-  bool isEmpty() const {
-    return topIndex_ == -1;
-  }
-
-  bool isFull() const {
-    return topIndex_ == kMaxSize - 1;
-  }
-
  private:
-  static const int kMaxSize = maxSize;
-  T data_[kMaxSize];
-  int topIndex_;
+  T storage[size] = { 100 };
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+  bool isFull() const {
+      return topIndex >= size - 1;
+  }
+  void push(const T& value) {
+    if (isFull()) {
+      throw std::runtime_error("Cannot push: stack is full");
+    }
+    storage[++topIndex] = value;
+  }
+  bool isEmpty() const {
+    return topIndex < 0;
+  }
+  void pop() {
+    if (isEmpty()) {
+      throw std::runtime_error("Cannot pop: stack is empty");
+    }
+    --topIndex;
+  }
+  const T& top() const {
+    if (isEmpty()) {
+      throw std::runtime_error("Cannot access top: stack is empty");
+    }
+    return storage[topIndex];
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
