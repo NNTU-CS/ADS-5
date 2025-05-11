65376c4a47fcb75ca46374dea58c5c4360c35ada
lesh <113383963+ilyaLeshenko@users.noreply.github.com>
2025-05-11 14:07:50 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,44 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T* arr;
+  int top;
+  int capacity;
+
+public:
+  TStack() : top(-1), capacity(size) {
+    arr = new T[capacity];
+  }
+
+  ~TStack() {
+    delete[] arr;
+  }
+
+  void push(const T& value) {
+    if (top == capacity - 1) {
+      throw "Stack overflow!";
+    }
+    arr[++top] = value;
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      std::cerr << "Error: stack underflow!\n";
+    }
+    return arr[top--];
+  }
+
+  T peek() const {
+    if (isEmpty()) {
+      std::cerr << "Error: stack is empty!\n";
+    }
+    return arr[top];
+  }
+
+  bool isEmpty() const {
+    return top == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
