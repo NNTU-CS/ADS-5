0affe19040e57daf59f2c73e935cb9a951c82af1
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-05-15 12:21:50 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,39 +2,48 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <stdexcept>
+
 template<typename T, int size>
 class TStack {
-  private: 
-    T data[size];
-    int top;
+ private:
+  T data[size];
+  int top;
 
-public: 
-    TStack() : top(-1) {}
+ public:
+  TStack() : top(-1) {}
 
-    void push(const T& value) {
-        if (top >= size - 1) {
-            throw std::overflow_error("Stack overflow");
-        }
-        data[++top] = value;
+  bool is_empty() const {
+    return top == -1;
+  }
+
+  bool is_full() const {
+    return top == size - 1;
+  }
+
+  void push(const T& value) {
+    if (!is_full()) {
+      data[++top] = value;
+    } else {
+      throw std::overflow_error("Stack is full");
     }
+  }
 
-    T pop() {
-        if (top < 0) {
-            throw std::underflow_error("Stack underflow");
-        }
-        return data[top--];
+  T pop() {
+    if (!is_empty()) {
+      return data[top--];
+    } else {
+      throw std::underflow_error("Stack is empty");
     }
+  }
 
-    T peek() const {
-        if (top < 0) {
-            throw std::underflow_error("Stack is empty");
-        }
-        return data[top];
+  T& top() {
+    if (!is_empty()) {
+      return data[top];
+    } else {
+      throw std::underflow_error("Stack is empty");
     }
-
-    bool isEmpty() const {
-        return top < 0;
-    }
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
