72c3001f3c2a634b3952752642c9ba99ca1c105d
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-05-15 13:04:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,9 +27,9 @@ std::string infx2pstfx(const std::string& inf) {
         output += stack1.pop();
         output += " ";
       }
-      stack1.pop(); // Удаляем '('
-    } else { // Оператор
-      while (!stack1.isEmpty() && 
+      stack1.pop();
+    } else {
+      while (!stack1.isEmpty() &&
              precedence(stack1.peek()) >= precedence(token[0])) {
         output += stack1.pop();
         output += " ";
@@ -54,18 +54,18 @@ int eval(const std::string& post) {
   while (iss >> token) {
     if (std::isdigit(token[0])) {
       stack2.push(std::stoi(token));
-    } else { // Оператор
+    } else {
       int right = stack2.pop();
       int left = stack2.pop();
       switch (token[0]) {
         case '+': stack2.push(left + right); break;
         case '-': stack2.push(left - right); break;
         case '*': stack2.push(left * right); break;
-        case '/': 
+        case '/':
           if (right == 0) {
             throw std::runtime_error("Division by zero");
           }
-          stack2.push(left / right); 
+          stack2.push(left / right);
           break;
       }
     }
