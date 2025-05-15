49642f1b68d1c65e447b294dcda3c63b1706f3e8
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-05-15 12:11:37 +0300

    f

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,68 @@
+// Copyright 2025 NNTU-CS
+#include <string>
+#include <map>
+#include <sstream>
+#include <stack>
+#include "tstack.h"
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+std::string infx2pstfx(const std::string& inf) {
+    std::string output;
+    TStack<char, 100> stack1; // Стек для операторов
+    std::istringstream iss(inf);
+    std::string token;
+
+    while (iss >> token) {
+        if (isdigit(token[0])) { // Если это число
+            output += token + " ";
+        } else if (token[0] == '(') {
+            stack1.push(token[0]);
+        } else if (token[0] == ')') {
+            while (!stack1.is_empty() && stack1.top() != '(') {
+                output += stack1.pop() + " ";
+            }
+            stack1.pop(); // Удаляем '('
+        } else { // Оператор
+            while (!stack1.is_empty() && precedence(stack1.top()) >= precedence(token[0])) {
+                output += stack1.pop() + " ";
+            }
+            stack1.push(token[0]);
+        }
+    }
+
+    while (!stack1.is_empty()) {
+        output += stack1.pop() + " ";
+    }
+
+    return output;
+}
+
+int eval(const std::string& post) {
+    TStack<int, 100> stack2; // Стек для чисел
+    std::istringstream iss(post);
+    std::string token;
+
+    while (iss >> token) {
+        if (isdigit(token[0])) { // Если это число
+            stack2.push(std::stoi(token));
+        } else { // Оператор
+            int b = stack2.pop();
+            int a = stack2.pop();
+            switch (token[0]) {
+                case '+': stack2.push(a + b); break;
+                case '-': stack2.push(a - b); break;
+                case '*': stack2.push(a * b); break;
+                case '/': stack2.push(a / b); break;
+            }
+        }
+    }
+
+    return stack2.pop(); // Возвращаем результат
+}
+
 
