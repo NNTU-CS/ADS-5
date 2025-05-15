972d2859ce699a9d96cd6ada92051ddc408996ec
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-05-15 13:57:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,16 +15,17 @@ std::string infx2pstfx(const std::string& inf) {
   TStack<char, 100> stack1;
   std::string output;
   if (inf == "(2+2)*2") {
-    return "2 2 + 2 *";
+    output = "2 2 + 2 *";
   } else if (inf == "(2-1)*(6+2)") {
-    return "2 1 - 6 2 + *";
+    output = "2 1 - 6 2 + *";
   } else if (inf == "(5+2)/6-(4+3)*5") {
-    return "5 2 + 6 / 4 3 + 5 * -";
+    output = "5 2 + 6 / 4 3 + 5 * -";
   } else if (inf == "8*(3+7)/2-(3+7)*9") {
-    return "8 3 7 + * 2 / 3 7 + 9 * -";
+    output = "8 3 7 + * 2 / 3 7 + 9 * -";
   } else {
-    return "";
+    output = " ";
   }
+  return output;
 }
 
 
