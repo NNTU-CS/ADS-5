5075fbc5c292c82005f6799a296392ad278c922d
KMasha04 <k.masha04030420@gmail.com>
2025-05-16 20:14:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,32 +12,32 @@ TStack<int, 100> stack2;
 std::string infx2pstfx(const std::string& inf) {
   std::string str = "";
   for (int i = 0; i < inf.length(); i++) {
-    if (inf[i] >= '0' && inf[i] <= '9') {
+    if (isdigit(inf[i])) {
       str += inf[i];
       str += ' ';
     } else {
-      if (inf[i] == '(' || stack1.isEmp() == 1 || 
-        priority[inf[i]] > priority[stack1.get()]) {
-        stack1.push(inf[i]);
+      if (inf[i] == '(') {
+        stack1.push('(');
       } else if (inf[i] == ')') {
-        while (stack1.get() != '(') {
-          str = str + stack1.pop() + ' ';
+        while (!stack1.isEmp() && stack1.get() != '(') {
+          str += stack1.pop();
+          str += ' ';
         }
-        if (stack1.get() == '(') {
-          stack1.pop();
+        if (!stack1.isEmp() && stack1.get() == '(') {
+          stack1.pop(); // убираем '('
         }
-      } else if (priority[inf[i]] <= priority[stack1.get()]) {
-        char item = stack1.pop();
-        str = str + item + ' ';
+      } else {
+        while (!stack1.isEmp() && priority[inf[i]] <= priority[stack1.get()]) {
+          str += stack1.pop();
+          str += ' ';
+        }
         stack1.push(inf[i]);
       }
     }
   }
-  while (stack1.isEmp() != 1) {
-    str = str + stack1.pop();
-    if (stack1.isEmp() != 1) {
-      str = str + ' ';
-    }
+  while (!stack1.isEmp()) {
+    str += stack1.pop();
+    str += ' ';
   }
 
   return str;
