b4f014760c1222cd78fa314c8a406ccc9dc0b800
kuzzmina <113035696+kuzzmina@users.noreply.github.com>
2025-05-16 20:46:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,86 +1,91 @@
 // Copyright 2025 NNTU-CS
-#include <string>
 #include <cctype>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
 #include "tstack.h"
 
-int precedence(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
-    return 0;
+static int precedence(char op) {
+  return (op == '+' || op == '-') ? 1 : (op == '*' || op == '/') ? 2 : 0;
+}
+static inline bool isOp(char c) {
+  return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
 std::string infx2pstfx(const std::string& inf) {
-    TStack<char, 100> stack;
-    std::string output;
-    size_t i = 0;
-    while (i < inf.size()) {
-        if (std::isspace(inf[i])) {
-            ++i;
-            continue;
-        }
-        if (std::isdigit(inf[i])) {
-            while (i < inf.size() && std::isdigit(inf[i])) {
-                output += inf[i++];
-            }
-            output += ' ';
-        } else if (inf[i] == '(') {
-            stack.push('(');
-            ++i;
-        } else if (inf[i] == ')') {
-            while (!stack.isEmpty() && stack.top() != '(') {
-                output += stack.top();
-                output += ' ';
-                stack.pop();
-            }
-            if (!stack.isEmpty() && stack.top() == '(') stack.pop();
-            ++i;
-        } else {
-            while (!stack.isEmpty() && precedence(stack.top()) >= precedence(inf[i])) {
-                output += stack.top();
-                output += ' ';
-                stack.pop();
-            }
-            stack.push(inf[i]);
-            ++i;
-        }
+  std::string out;
+  TStack<char, 128> ops;
+
+  for (size_t i = 0; i < inf.size(); ++i) {
+    char ch = inf[i];
+
+    if (std::isspace(static_cast<unsigned char>(ch))) continue;
+
+    if (std::isdigit(static_cast<unsigned char>(ch))) {
+      while (i < inf.size() && std::isdigit(static_cast<unsigned char>(inf[i])))
+        out += inf[i++];
+      out += ' ';
+      --i;
+    } else if (ch == '(') {
+      ops.push(ch);
+    } else if (ch == ')') {
+      while (!ops.empty() && ops.top() != '(') {
+        out += ops.top();
+        out += ' ';
+        ops.pop();
+      }
+      if (!ops.empty()) ops.pop();
+    } else if (isOp(ch)) {
+      while (!ops.empty() && isOp(ops.top()) &&
+             precedence(ops.top()) >= precedence(ch)) {
+        out += ops.top();
+        out += ' ';
+        ops.pop();
+      }
+      ops.push(ch);
     }
-    while (!stack.isEmpty()) {
-        output += stack.top();
-        output += ' ';
-        stack.pop();
-    }
-    if (!output.empty() && output.back() == ' ') output.pop_back();
-    return output;
+  }
+  while (!ops.empty()) {
+    out += ops.top();
+    out += ' ';
+    ops.pop();
+  }
+  if (!out.empty() && out.back() == ' ') out.pop_back();
+  return out;
 }
 
 int eval(const std::string& post) {
-    TStack<int, 100> stack;
-    size_t i = 0;
-    while (i < post.size()) {
-        if (std::isspace(post[i])) {
-            ++i;
-            continue;
-        }
-        if (std::isdigit(post[i])) {
-            int val = 0;
-            while (i < post.size() && std::isdigit(post[i])) {
-                val = val * 10 + (post[i] - '0');
-                ++i;
-            }
-            stack.push(val);
-        } else {
-            int b = stack.top(); stack.pop();
-            int a = stack.top(); stack.pop();
-            int res = 0;
-            switch (post[i]) {
-                case '+': res = a + b; break;
-                case '-': res = a - b; break;
-                case '*': res = a * b; break;
-                case '/': res = a / b; break;
-            }
-            stack.push(res);
-            ++i;
-        }
+  std::stringstream ss(post);
+  std::string token;
+  TStack<int, 128> st;
+
+  while (ss >> token) {
+    if (token.size() == 1 && isOp(token[0])) {
+      if (st.count() < 2)
+        throw std::runtime_error("incorrect postfix expression");
+      int rhs = st.top();
+      st.pop();
+      int lhs = st.top();
+      st.pop();
+      switch (token[0]) {
+        case '+':
+          st.push(lhs + rhs);
+          break;
+        case '-':
+          st.push(lhs - rhs);
+          break;
+        case '*':
+          st.push(lhs * rhs);
+          break;
+        case '/':
+          st.push(lhs / rhs);
+          break;
+      }
+    } else {
+      st.push(std::stoi(token));
     }
-    return stack.top();
+  }
+  if (st.count() != 1) throw std::runtime_error("incorrect postfix expression");
+  return st.top();
 }
