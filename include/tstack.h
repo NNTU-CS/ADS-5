db2ab33194cb9bedd3702fd4c2f93ff7a796df56
HardRandom <90847034+HardRandom@users.noreply.github.com>
2025-05-16 21:17:06 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,43 +2,32 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  public:
-  TStack() : pointer(-1) {}
-
-  bool isVoid() const {
-    return pointer == -1;
-  }
-
-  bool isFull() const {
-    return pointer == Capacity - 1;
-  }
-
-  void add(const ElementType& value) {
-    if (isFull()) {
-      throw std::overflow_error("Stack overflow");
+public:
+    TStack() : pointer(-1) {}
+    
+    bool isVoid() const { return pointer == -1; }
+    bool isFull() const { return pointer == size - 1; }
+    
+    void add(const T& value) {
+        if (isFull()) throw std::overflow_error("Stack overflow");
+        storage[++pointer] = value;
     }
-    storage[++pointer] = value;
-  }
-
-  ElementType remove() {
-    if (isVoid()) {
-      throw std::underflow_error("Stack underflow");
+    
+    T remove() {
+        if (isVoid()) throw std::underflow_error("Stack underflow");
+        return storage[pointer--];
     }
-    return storage[pointer--];
-  }
-
-  ElementType getTop() const {
-    if (isVoid()) {
-      throw std::underflow_error("Stack empty");
+    
+    T getTop() const {
+        if (isVoid()) throw std::underflow_error("Stack empty");
+        return storage[pointer];
     }
-    return storage[pointer];
-  }
 
- private:
-  ElementType storage[Capacity];
-  int pointer;
+private:
+    T storage[size];
+    int pointer;
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
