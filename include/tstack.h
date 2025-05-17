72bcf61a42ff4d3195fa450197c9f30ec4196d1d
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-05-17 15:01:27 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,42 +1,45 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <iostream>
 
-template<typename T, int kSize>
+template<typename T, int kStackSize>
 class TStack {
-  T data[kSize];
+ private:
+  T data[kStackSize];
   int topIndex;
-
  public:
   TStack() : topIndex(-1) {}
-
-  void push(const T& value) {
-    if (topIndex < kSize - 1) {
-      data[++topIndex] = value;
-    }
-  }
-
-  void pop() {
-    if (topIndex >= 0) {
-      --topIndex;
-    }
-  }
-
-  T peek() const {
-    if (topIndex >= 0) {
-      return data[topIndex];
-    }
-    throw std::underflow_error("Stack is empty");
-  }
-
-  T top() const {
-    return data[topIndex];
-  }
-
   bool isEmpty() const {
     return topIndex == -1;
+  }
+  bool isFull() const {
+    return topIndex == kStackSize - 1;
+  }
+  void push(const T& element) {
+    if (isFull()) {
+      std::cerr << "Stack Overflow!" << std::endl;
+      return;
+    }
+    data[++topIndex] = element;
+  }
+  T pop() {
+    if (isEmpty()) {
+      std::cerr << "Stack Underflow!" << std::endl;
+      return T();
+    }
+    return data[topIndex--];
+  }
+  T top() const {
+    if (isEmpty()) {
+      std::cerr << "Stack is Empty!" << std::endl;
+      return T();
+    }
+    return data[topIndex];
+  }
+  int get_size() const {
+    return kStackSize;
   }
 };
 
 #endif  // INCLUDE_TSTACK_H_
-
