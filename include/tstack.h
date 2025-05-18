5b52ed5bfac39e87a071769ca04fb2e4d7d9ccad
nitron1108 <mja29122@gmail.com>
2025-05-18 18:47:03 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,46 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+#include <stdexcept>
+
+template<typename T, int MaxSize>
 class TStack {
-  // добавьте код стека
+public:
+    TStack() : topIndex(-1) {}
+
+    bool empty() const {
+        return topIndex == -1;
+    }
+
+    bool full() const {
+        return topIndex == MaxSize - 1;
+    }
+
+    void push(const T& value) {
+        if (full()) {
+            throw std::overflow_error("Stack overflow");
+        }
+        storage[++topIndex] = value;
+    }
+
+    T pop() {
+        if (empty()) {
+            throw std::underflow_error("Stack underflow");
+        }
+        return storage[topIndex--];
+    }
+
+    T peek() const {
+        if (empty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return storage[topIndex];
+    }
+
+private:
+    T storage[MaxSize];
+    int topIndex;
 };
 
 #endif  // INCLUDE_TSTACK_H_
