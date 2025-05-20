e42e9e55789c3fc451e6b47860f256d6aeb44489
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-05-20 07:41:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,78 +1,103 @@
 // Copyright 2025 NNTU-CS
-#include <string>
+#include <iostream>
 #include <sstream>
-#include <cctype>
+#include <stdexcept>
+#include <string>
+#include <map>
 #include "tstack.h"
-
-using std::string;
-using std::stringstream;
-using std::isdigit;
-
-static int precedence(char op) {
-  if (op == '+' || op == '-') return 1;
-  if (op == '*' || op == '/') return 2;
-  return 0;
-}
-
-string infx2pstfx(const string& inf) {
-  TStack<char, 100> stack;
-  string postfix;
-  for (size_t i = 0; i < inf.size();) {
-    char c = inf[i];
-    if (isdigit(c)) {
-      string num;
-      while (i < inf.size() && isdigit(inf[i])) {
-        num += inf[i];
-        i++;
-      }
-      postfix += num + " ";
-    } else if (c == '(') {
-      stack.Push(c);
-      i++;
-    } else if (c == ')') {
-      while (!stack.IsEmpty() && stack.Top() != '(') {
-        postfix += stack.Pop();
-        postfix += ' ';
-      }
-      stack.Pop();
-      i++;
-    } else {
-      while (!stack.IsEmpty() && precedence(c) <= precedence(stack.Top())) {
-        postfix += stack.Pop();
-        postfix += ' ';
-      }
-      stack.Push(c);
-      i++;
+int prior(char op) {
+    switch (op) {
+        case '+': case '-': return 1;
+        case '*': case '/': return 2;
+        default:
+            return 0;
     }
-  }
-  while (!stack.IsEmpty()) {
-    postfix += stack.Pop();
-    postfix += ' ';
-  }
-  if (!postfix.empty() && postfix.back() == ' ') {
-     postfix.pop_back();
-  }
-  return postfix;
 }
-
-int eval(const string& post) {
-  TStack<int, 100> stack;
-  stringstream ss(post);
-  string token;
-  while (ss >> token) {
-    if (isdigit(token[0])) {
-      stack.Push(stoi(token));
-    } else {
-      int b = stack.Pop();
-      int a = stack.Pop();
-      switch (token[0]) {
-        case '+': stack.Push(a + b); break;
-        case '-': stack.Push(a - b); break;
-        case '*': stack.Push(a * b); break;
-        case '/': stack.Push(a / b); break;
-      }
+bool opperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+std::string infx2pstfx(const std::string& inf) {
+    TStack<char, 100> st1;
+    std::ostringstream finall;
+    bool flag = false;
+    for (size_t i = 0; i < inf.size(); ++i) {
+        char c = inf[i];
+        if (c == ' ') continue;
+        if (isdigit(c)) {
+            if (flag) {
+                finall << c;
+            } else {
+                if (!finall.str().empty()) finall << ' ';
+                finall << c;
+            }
+            flag = true;
+        } else {
+                flag = false;
+            if (c == '(') {
+                st1.push(c);
+            } else if (c == ')') {
+                while (!st1.isEmpty() && st1.peek() != '(') {
+                    finall << ' ' << st1.pop();
+                }
+                if (!st1.isEmpty()) st1.pop();
+            } else if (opperator(c)) {
+                while (!st1.isEmpty() && st1.peek() != '(' &&
+                    prior(c) <= prior(st1.peek())) {
+                    finall << ' ' << st1.pop();
+                }
+                st1.push(c);
+            }
+        }
     }
-  }
-  return stack.Pop();
+    while (!st1.isEmpty()) {
+        finall << ' ' << st1.pop();
+    }
+    return finall.str();
 }
-
+int eval(const std::string& pref) {
+    TStack<int, 100> stk2;
+    std::istringstream stream(pref);
+    std::string token;
+    while (stream >> token) {
+        if (std::isdigit(token[0])) {
+            stk2.push(std::stoi(token));
+        } else if (opperator(token[0]) && token.size() == 1) {
+            if (stk2.isEmpty()) {
+                throw std::invalid_argument("Not enough operandov");
+            }
+            int oper2 = stk2.pop();
+            if (stk2.isEmpty()) {
+                throw std::invalid_argument("Not enough operandov");
+            }
+            int oper1 = stk2.pop();
+            switch (token[0]) {
+                case '+': 
+                case '+':
+                    stk2.push(oper1 + oper2);
+                    break;
+                case '-': 
+                case '-':
+                    stk2.push(oper1 - oper2);
+                    break;
+                case '*': 
+                case '*':
+                    stk2.push(oper1 * oper2);
+                    break;
+                case '/':
+                    if (oper2 == 0) {
+                        throw std::runtime_error("Division by 0");
+                    }
+                stk2.push(oper1 / oper2);
+                break;
+            }
+        }
+    }
+    if (stk2.isEmpty()) {
+        throw std::invalid_argument("Empty expression");
+    }
+    int result = stk2.pop();
+    if (!stk2.isEmpty()) {
+        throw std::invalid_argument("Many operands");
+    }
+    return result;
+}
